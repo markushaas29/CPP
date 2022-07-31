9c270dd6539928c41eca7aa9e2a39f93c1d3ccb5
markushaas29 <markushaas1@gmx.de>
2022-07-31 15:54:26 +0200

    RefactorStagetest

--- Home/Stagetest.cpp
+++ Home/Stagetest.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <boost/mpl/vector.hpp>
 #include "Counter.hpp"
 #include "CounterContainer.hpp"
 #include "Stage.hpp"
-#include "House.hpp"
 #include "StageContainer.hpp"
+#include "House.hpp"
+#include "AncilliaryRentalCosts/AncilliaryRentalCostsContainer.hpp"
 #include "../Unit/SIPrefix.hpp"
 #include "../Calculator/Calculator.hpp"
 #include "../Tuple/Tuple.hpp"
@@ -17,7 +17,6 @@ int main()
     std::cout<<"START"<<std::endl;
 	
 	House<HouseConfiguration>::Instance();
-// 	Top::Instance();
     std::cout<<"t: "<<Top::Name<<std::endl;
     std::cout<<"t: "<<Top::Number<<std::endl;
     
@@ -27,22 +26,21 @@ int main()
 	Rooms r(6);
 	
 	StageContainerType::Instance().Display(std::cout);
-// 	auto m = StageContainerType::Instance().Get<Middle>();
     std::cout<<"m: "<<Middle::Instance().Name<<std::endl;
     
     std::cout<<"AREA"<<std::endl;
-	std::cout<<"t: "<<GetStage<ApartmentArea,Top::Configuration>().GetQuantity()<<std::endl;
-    std::cout<<"m: "<<GetStage<ApartmentArea,Middle::Configuration>().GetQuantity()<<std::endl;
-    std::cout<<"b: "<<GetStage<ApartmentArea,Bottom::Configuration>().GetQuantity()<<std::endl;
+	std::cout<<"t: "<<GetStage<Top,ApartmentArea>()<<std::endl;
+    std::cout<<"m: "<<GetStage<Middle,ApartmentArea>().GetQuantity()<<std::endl;
+    std::cout<<"b: "<<GetStage<Bottom,ApartmentArea>().GetQuantity()<<std::endl;
     std::cout<<"TOTAL: "<<StageContainerType::Instance().GetTotal<ApartmentArea>()<<std::endl;
 	
     std::cout<<"\nPersons"<<std::endl;
-	std::cout<<"t: "<<GetStage<Persons,Top::Configuration>().GetQuantity()<<std::endl;
-    std::cout<<"m: "<<GetStage<Persons,Middle::Configuration>().GetQuantity()<<std::endl;
-    std::cout<<"b: "<<GetStage<Persons,Bottom::Configuration>().GetQuantity()<<std::endl;
+	std::cout<<"t: "<<GetStage<Top,Persons>().GetQuantity()<<std::endl;
+    std::cout<<"m: "<<GetStage<Middle,Persons>().GetQuantity()<<std::endl;
+    std::cout<<"b: "<<GetStage<Bottom,Persons>().GetQuantity()<<std::endl;
     std::cout<<"TOTAL: "<<StageContainerType::Instance().GetTotal<Persons>()<<std::endl;
 	
-	StageContainerType::Instance().Calculate();
+	StageContainerType::Instance().Calculate<AncilliaryRentalCostsContainer>();
     std::cout<<"END"<<std::endl;
 
     return 0;
