2f0052d141c982ee3d0c22a82c08b8b42b81e8a0
markushaas29 <markushaas1@gmx.de>
2022-09-18 13:37:17 +0200

    templateIterator

--- File/RepositoryObjectVisitor.hpp
+++ File/RepositoryObjectVisitor.hpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <functional>
+#include "../String/String_.hpp"
 
 #ifndef REPOSITORYOBJECTVISITOR_HPP
 #define REPOSITORYOBJECTVISITOR_HPP
@@ -8,18 +9,19 @@ namespace fs = std::filesystem;
 
 namespace FS
 {
+	template<typename Iterator = std::vector<std::string>::const_iterator>
 	class RepositoryObjectVisitor
 	{
-		
 		std::string name;
 		std::function<bool(std::string)> is;
+		std::function<void(Iterator,Iterator)> parse;
 	public:
 		RepositoryObjectVisitor() = delete;
-		RepositoryObjectVisitor(std::string n, std::function<bool(std::string)> is_): name{n}, is{is_} {};
+		RepositoryObjectVisitor(std::string n, std::function<bool(std::string)> is_ = [](const std::string& s){ return String_::Contains(name,s); }): name{n}, is{is_} {};
 		std::string Name() { this->is(name) ;return this->name;}
 	};
 	
-	std::ostream& operator<<(std::ostream& out, const RepositoryObjectVisitor& c)	{ return out;	}
+	//~ std::ostream& operator<<(std::ostream& out, const RepositoryObjectVisitor& c)	{ return out;	}
 
 }
 #endif
