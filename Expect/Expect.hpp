3a1efdfa1191487d64a51d0c13bc9dcd7b78e3e1
markus <markus@gmx.de>
2022-11-13 16:48:34 +0100

    Expect

---
+++ Expect/Expect.hpp
@@ -0,0 +1,11 @@
+#include "../Logger/Logger.hpp"
+#include "../Wrapper/Wrapper.hpp"
+
+#ifndef EXPECT_HPP
+#define EXPECT_HPP
+
+class Expect
+{  
+};
+
+#endif
---
+++ Expect/Test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <typeinfo>
+#include <typeindex>
+#include "Expect.hpp"
+
+
+int main()
+{       
+    std::cout<<"START"<<std::endl;
+	
+		
+	std::cout<<"END"<<std::endl;
+
+    return 0;
+}
