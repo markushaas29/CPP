cf027e05258d5a063ec2eba740060a1b622dfc99
markus <markus@gmx.de>
2022-12-17 17:13:29 +0100

    ParseResult

--- String/To/To.hpp
+++ String/To/To.hpp
@@ -4,6 +4,35 @@
 
 namespace String_
 {
+
+template<typename Target=std::string>
+struct ParseResult
+{
+	ParseResult(Target t): Valid{true}, Value{t} {};
+	ParseResult(bool v = false): Valid{v}, Value{} {};
+	Target Value;
+	bool Valid;
+	operator Target () { return Value; }
+	operator bool () { return Valid; }
+	
+	std::ostream& Display(std::ostream& out) const	{	return out<<"Valid: "<<Valid<<"\t Value:"<<Value; }
+};
+
+
+template<typename T>
+std::ostream& operator<<(std::ostream& strm, const ParseResult<T>& p){	return p.Display(strm);}
+
+template<typename Target=std::string, typename Source=std::string>
+decltype(auto) TryTo(Source arg)
+{
+	std::stringstream buf;
+	Target result;
+	
+	if(!(buf << arg) || !(buf >> result) || !(buf >> std::ws).eof())
+		return ParseResult<Target>();
+	return ParseResult<Target>(result);
+}
+
 	
 template<typename Target=std::string, typename Source=std::string>
 Target ParseTo(Source arg)
--- String/To/ToTest.cpp
+++ String/To/ToTest.cpp
@@ -2,13 +2,20 @@
 #include <vector>
 #include "../../Logger/Logger.hpp"
 #include "To.hpp"
+using namespace String_;
 
 int main()
 {       
-	std::cout<<"1.2_"<<To("1.2")<<std::endl;
-	std::cout<<"1.2_"<<To(1.2)<<std::endl;
-	std::cout<<"1.2_"<<To<double,int>(1.2)<<std::endl;
-	std::cout<<"1.2_"<<To<double,char>(1.2)<<std::endl;
+	std::cout<<"1.2_"<<ParseTo("1.2")<<std::endl;
+	std::cout<<"1.2_"<<ParseTo(1.2)<<std::endl;
+	std::cout<<"1.2_"<<ParseTo<double,int>(1.2)<<std::endl;
+	double d = TryTo<double>("1.2");
+	std::cout<<"1.2_d "<<d<<std::endl;
+
+	std::cout<<"1.2_"<<TryTo<double,char>(1.2)<<" "<<std::endl;
+	if(!TryTo<double,char>(1.2))
+		std::cout<<"1.2_TryTo<double,char>(1.2) false "<<std::endl;
+	std::cout<<"1.2_"<<ParseTo<double,char>(1.2)<<" "<<std::endl;
 
 	std::cout<<"END"<<std::endl;
     
