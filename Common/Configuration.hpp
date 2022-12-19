d51ba079c8af81d80781604f022acab1f8fa1e9d
markus <markus@gmx.de>
2022-12-19 15:04:49 +0100

    Configuration

--- Common/Configuration.hpp
+++ Common/Configuration.hpp
@@ -3,11 +3,20 @@
 template<typename Derived>
 struct ConfigurationBase
 {
-	Type = Derived
+	using Type = Derived;
 };
 
-template<typename T>
+template<class T>
 struct Configuration: ConfigurationBase<Configuration<T>>
 {
 	
+};
+
+template<typename S> class AncilliaryRentalCostsContainer;
+template<typename ConfigT> class Stage;
+
+template<typename T>
+struct Configuration<AncilliaryRentalCostsContainer<T>>: ConfigurationBase<Configuration<AncilliaryRentalCostsContainer<T>>>
+{
+	using Stage = T;
 };
