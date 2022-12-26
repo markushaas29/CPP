c1f58310a5d744ebd2da7ca96518b0efef721d92
markus <markus@gmx.de>
2022-12-26 06:12:11 +0100

    ToPoint

--- String/To/To.hpp
+++ String/To/To.hpp
@@ -1,9 +1,14 @@
 #include <sstream>
+#include "../../Logger/Logger.hpp"
+#include "../../Wrapper/Wrapper.hpp"
 
 #pragma once
 
 namespace String_
 {
+
+using Comma = T::char_<','>;
+using Point = T::char_<'.'>;
 
 template<typename Target=std::string>
 struct ParseResult
@@ -37,6 +42,12 @@ decltype(auto) TryTo(Source arg)
 template<typename Target=std::string, typename Source=std::string>
 Target ParseTo(Source arg)
 {
+	if constexpr (std::is_same_v<Target,double> && std::is_same_v<Source,std::string>)
+	{
+		Logger::Log("String_::ParseTo: comma by point in ", arg);
+		std::replace( arg.begin(), arg.end(), Comma::Value, Point::Value);
+	}
+	
 	auto result = TryTo<Target,Source>(arg);
 
 	if(!result.Valid)
--- String/To/ToTest.cpp
+++ String/To/ToTest.cpp
@@ -10,7 +10,12 @@ int main()
 	std::cout<<"1.2_"<<ParseTo(1.2)<<std::endl;
 	std::cout<<"1.2_"<<ParseTo<double,int>(1.2)<<std::endl;
 	double d = TryTo<double>("1.2");
-	std::cout<<"1.2_d "<<d<<std::endl;
+	std::string arg = "1,3";
+	std::replace( arg.begin(), arg.end(), Comma::Value, Point::Value);
+	std::cout<<"arg_"<<arg<<" "<<std::endl;
+	
+	std::cout<<"1,3_"<<ParseTo<double,std::string>("1,3")<<" "<<std::endl;
+	std::cout<<"1,4_"<<ParseTo<double,std::string>("1,4")<<" "<<std::endl;
 
 	std::cout<<"\"\"""_"<<TryTo<double>("")<<" "<<std::endl;
 	std::cout<<"1.2_"<<TryTo<double,char>(1.2)<<" "<<std::endl;
