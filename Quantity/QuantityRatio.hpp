b8a8c310cc74ab9ba36b8b746660becce4a7eb36
markus <markus@gmx.de>
2023-01-03 15:24:14 +0100

    Pow

--- Quantity/QuantityRatio.hpp
+++ Quantity/QuantityRatio.hpp
@@ -1,17 +1,18 @@
 #include <memory>
 #include <ratio>
+#include "../Math/Math.hpp"
 
 #pragma once
 
 template<int a,int b,int c,int d,int e,int f,int g,int h> class Unit;
 
-template<uint N, uint D>
+template<uint N, uint D, int Ex>
 struct QuantityRatioBase
 {
-	using Type = QuantityRatioBase<N,D>;
+	using Type = QuantityRatioBase<N,D,Ex>;
 	
-	static constexpr uint Num = N;
-	static constexpr uint Denom = D;
+	static constexpr uint Num = Ex > 0 ? Math::Pow<N,Ex>::Result : 1;
+	static constexpr uint Denom = Ex < 0 ? Math::Pow<N,-Ex>::Result : 1;
 	static constexpr std::ratio<Num, Denom> Ratio = std::ratio<Num, Denom>();
 	static inline const std::string Sign;
 	static inline const std::string Name;
@@ -19,15 +20,15 @@ struct QuantityRatioBase
 };
 
 template<int N, typename U>
-struct QuantityRatio: public QuantityRatioBase<1000,1>
+struct QuantityRatio: public QuantityRatioBase<1000,1,N>
 {
-	using Base = QuantityRatioBase<1000,1>;
+	using Base = QuantityRatioBase<1000,1,N>;
 };
 
 template<int N>
-struct QuantityRatio<N,Unit<0,0,0,N,0,0,0,0>>: public QuantityRatioBase<60,1>
+struct QuantityRatio<N,Unit<0,0,0,1,0,0,0,0>>: public QuantityRatioBase<60,1,N>
 {
-	using Base = QuantityRatioBase<60,1>;
+	using Base = QuantityRatioBase<60,1,N>;
 };
 
 
--- Quantity/QuantityRatioTest.cpp
+++ Quantity/QuantityRatioTest.cpp
@@ -9,9 +9,15 @@ int main()
 
 	using L = QuantityRatio<1,Length>;
 	using T = QuantityRatio<1,Time>;
+    std::cout<<"L: "<<L::Num<<" / "<<L::Denom<<std::endl;
+    std::cout<<"T: "<<T::Num<<" / "<<T::Denom<<std::endl;
     assert(L::Num==1000);
     assert(T::Num==60);
 	
+	using L2 = QuantityRatio<2,Length>;
+	using T2 = QuantityRatio<2,Time>;
+    std::cout<<"L: "<<L2::Num<<" / "<<L2::Denom<<std::endl;
+    std::cout<<"T: "<<T2::Num<<" / "<<T2::Denom<<std::endl;
     std::cout<<"END"<<std::endl;
 
     return 0;
