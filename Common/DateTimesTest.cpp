bc1ba80b6f824a277ef5e5f3b668ebcaf85922d2
markus <markus@gmx.de>
2023-01-21 15:14:48 +0100

    DateTimesTest

--- Common/DateTimesTest.cpp
+++ Common/DateTimesTest.cpp
@@ -60,45 +60,36 @@ int main()
 	auto d2 = DateTimes::Date("28.2.2022");
 	
 	std::cout<<"Days "<<(d2 - d1)<<std::endl;
+	assert((d2 - d1).Value()==5);
 	std::cout<<"Days "<<(d1 - d2)<<std::endl;
+	assert((d1 - d2).Value()==5);
 
 	auto d3 = DateTimes::Date("5.3.2020");
 	auto d4 = DateTimes::Date("28.2.2020");
 	
 	std::cout<<"Days "<<(d3 - d1)<<std::endl;
+	assert((d3 - d1).Value()==730);
 	std::cout<<"Days "<<(d4 - d3)<<std::endl;
+	assert((d4 - d3).Value()==6);
 	std::cout<<d4<<" > "<<d3<<"\t"<<(d4 > d3)<<std::endl;
+	assert(!(d4 > d3));
 	std::cout<<d4<<"< "<<d3<<"\t"<<(d4 < d3)<<std::endl;
+	assert(d4 < d3);
 	std::cout<<d4<<"== "<<d3<<"\t"<<(d4 == d3)<<std::endl;
+	assert(d4 != d3);
 	std::cout<<d3<<"== "<<d3<<"\t"<<(d3 == d3)<<std::endl;
+	assert(d3 == d3);
 	std::cout<<DateTimes::July<<"== "<<DateTimes::July<<"\t"<<(DateTimes::July == DateTimes::July)<<std::endl;
+	assert(DateTimes::July== DateTimes::July);
 	auto y2021 = DateTimes::Year(2021);
 	auto y2020 = DateTimes::Year(2020);
 	std::cout<<"Leap 2020"<<"\t"<<y2020.IsLeap<<std::endl;
+	assert(y2020.IsLeap);
 	std::cout<<"Leap 2021"<<"\t"<<y2021.IsLeap<<std::endl;
+	assert(!y2021.IsLeap);
 	std::cout<<DateTimes::July<<"== "<<DateTimes::July<<"\t"<<(DateTimes::Year(2021) == DateTimes::Year(2021))<<std::endl;
+	assert((DateTimes::Year(2021) == DateTimes::Year(2021)));
 	
-	//~ auto d = Chars(18,12,2021);
-	//~ auto d = DateTimes::Date::getChars(18,12,2021);
-	//~ std::cout<<d[0]<<std::endl;
-	//~ std::cout<<d[1]<<std::endl;
-	//~ std::cout<<d[2]<<std::endl;
-	//~ std::cout<<d[3]<<std::endl;
-	//~ std::cout<<d[4]<<std::endl;
-	//~ std::cout<<d[5]<<std::endl;
-	//~ std::cout<<d[6]<<std::endl;
-	//~ std::cout<<d[7]<<std::endl;
-	//~ std::cout<<std::string(d.data())<<std::endl;
-	//~ std::cout<<d.Value()<<std::endl;
-	
-	//~ auto dt = DateTimes::Date(18,(uint)12,(uint)2021);
-	//~ std::cout<<dt.Value()<<std::endl;
-	//~ std::cout<<dt[0]<<std::endl;
-	//~ std::cout<<dt[4]<<std::endl;
-	//~ std::cout<<dt[9]<<std::endl;
-	//~ std::cout<<dt.Size()<<std::endl;
-	
-
 	std::cout<<"END"<<std::endl;
     
 	return 0;
