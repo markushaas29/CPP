60417f7f55942e198fa41043b170634d5a0200b3
markus <markus@gmx.de>
2023-01-22 14:18:19 +0100

    ReadingTest

--- Home/ReadingTest/ReadingTest.cpp
+++ Home/ReadingTest/ReadingTest.cpp
@@ -14,8 +14,17 @@ int main()
 	auto dt = DT{1,2,2003};
 	auto rw = RW{w,dt};
 	
+	auto w2 = W{124};
+	auto dt2 = DT{1,2,2004};
+	auto rw2 = RW{w2,dt2};
+	
     std::cout<<"Watt "<<rw<<std::endl;
 	assert(rw.QuantityValue == w);
 	assert(rw.Date == dt);
+	
+	auto drw = rw2 -rw;
+    //~ std::cout<<"Watt "<<drw<<std::endl;
+    std::cout<<"Watt "<<drw.Get()<<std::endl;
+	
     return 0;
 }
