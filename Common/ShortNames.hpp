8d9f2457ee736cc26a3204c365783e8a049b665a
markus <markus@gmx.de>
2023-01-22 13:39:43 +0100

    UsingSHortNames

---
+++ Common/ShortNames.hpp
@@ -0,0 +1,16 @@
+#include "DateTimes.hpp"
+#include "../CSV/Elements.hpp"
+
+#pragma once
+
+namespace ShortNames
+{
+	using I = IBAN;
+	using B = BIC;
+	using N = Name;
+	using DT = DateTimes::Date;
+	using Q = Quantity<Sum>;
+	using D = Bank::Direction;
+	using E = Entry;
+};
+
--- File/TransferTest/TransferTest.cpp
+++ File/TransferTest/TransferTest.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include "../Transfer.hpp"
 #include "../Raiffeisenbank.hpp"
-#include "../../Common/DateTimes.hpp"
-#include "../../CSV/Elements.hpp"
 #include "../../Common/Configuration.hpp"
+#include "../../Common/ShortNames.hpp"
+using namespace ShortNames;
 
 int main()
 {       
     std::cout<<"START"<<std::endl;
 	using A = Bank::Raiffeisenbank<Configuration::Raiffeisenbank>;
 	using TT = A::TransferType;
-	using TP = std::tuple<IBAN,BIC,Name,DateTimes::Date, Quantity<Sum>, Bank::Direction, Entry>;
+	using TP = std::tuple<I, B, N, DT, Q, D, E>;
 	
 	auto tp = std::make_tuple(IBAN{},BIC{}, DateTimes::Date{1,1,2000}, Quantity<Sum>{123}, Bank::Direction(), Entry{});
+	//~ TP tp1(I{},B{}, DT{1,1,2000}, Q{123}, D(), E{});
 	
     return 0;
 }
