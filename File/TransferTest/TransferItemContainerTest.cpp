3e1d3f94c8a9c374a21215319644514a38a40fde
markus <markus@gmx.de>
2023-01-28 15:22:38 +0100

    TransferItemCont

---
+++ File/TransferTest/TransferItemContainerTest.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "../Transfer.hpp"
+#include "../Raiffeisenbank.hpp"
+#include "../../Common/Configuration.hpp"
+#include "../../Common/ShortNames.hpp"
+#include "../../Common/DateTimes.hpp"
+using namespace ShortNames;
+
+int main()
+{       
+    std::cout<<"START"<<std::endl;
+	using A = Bank::Raiffeisenbank<Configuration::Raiffeisenbank>;
+	using TT = A::TransferType;
+	using TP = std::tuple<I, B, N, DT, Q, D, E>;
+	
+	auto i = I{"DE3"};
+	auto b = B{"COBA"};
+	auto n = N{"MN"};
+	auto dt = DT{5,3,2022};
+	auto q = Q{123};
+	auto d = D();
+	auto e = E{"TEST"};
+	
+	auto tp = std::make_tuple(i,b,n,dt,q,d,e);
+	auto t = std::make_shared<TT>(tp);
+	
+	auto tc = TC{};
+	tc.Add(t);
+	std::cout<<(**tc.Begin())<<std::endl;
+	assert(tc.Size()==1);
+	
+    return 0;
+}
