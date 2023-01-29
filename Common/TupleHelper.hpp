809491fd05f470713b025955c45fb6decc2705ba
markus <markus@gmx.de>
2023-01-29 14:10:53 +0100

    TupleHelper

---
+++ Common/TupleHelper.hpp
@@ -0,0 +1,19 @@
+#include <tuple>
+#include <type_traits>
+
+#pragma once
+
+template <typename T, typename Tuple>
+struct has_type;
+
+template <typename T>
+struct has_type<T, std::tuple<>> : std::false_type {};
+
+template <typename T, typename U, typename... Ts>
+struct has_type<T, std::tuple<U, Ts...>> : has_type<T, std::tuple<Ts...>> {};
+
+template <typename T, typename... Ts>
+struct has_type<T, std::tuple<T, Ts...>> : std::true_type {};
+	
+template <typename T, typename Tuple>
+using tuple_contains_type = typename has_type<T, Tuple>::type;
