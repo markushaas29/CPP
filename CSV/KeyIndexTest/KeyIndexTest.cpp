984cc56314ed397790cac1ee9fa0e821cface1c6
markus <markus@gmx.de>
2023-02-11 14:51:05 +0100

    RemoveEquality

--- CSV/KeyIndexTest/KeyIndexTest.cpp
+++ CSV/KeyIndexTest/KeyIndexTest.cpp
@@ -9,9 +9,11 @@ int main()
     std::cout<<"START"<<std::endl;
     
     auto k1 = Key("1");
+    auto k_1 = Key("1");
     auto k12 = Key("12");
     std::cout<<k1<<std::endl;
     assert(k1=="1");
+    assert(k1==k_1);
     assert(k1!=k12);
     assert(k1<k12);
 	
