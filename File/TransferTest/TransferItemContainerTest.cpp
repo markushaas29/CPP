43bda7b03717b4661c2367d258bca9c23d2b8afc
markus <markus@gmx.de>
2023-02-19 15:49:04 +0100

    ReadTransactionContainerTest

--- File/TransferTest/TransferItemContainerTest.cpp
+++ File/TransferTest/TransferItemContainerTest.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "../Transfer.hpp"
+#include "../Node.hpp"
 #include "../Raiffeisenbank.hpp"
 #include "../../Common/Configuration.hpp"
 #include "../../Common/ShortNames.hpp"
@@ -10,7 +11,8 @@ int main()
 {       
     std::cout<<"START"<<std::endl;
 	
-	TI::Instance();
+	auto keys = FS::ReadLines("/home/markus/Downloads/CSV_Files/Raiba.keys");
+	TI::Instance().UpdateKeyIndices(keys.cbegin(), keys.end());
 	
     return 0;
 }
