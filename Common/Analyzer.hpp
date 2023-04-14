16d26ecc724f0003969fe1a2a8d43dacdc74c8f6
markus <markus@gmx.de>
2023-04-14 15:58:03 +0200

    ConceptAna

--- Common/Analyzer.hpp
+++ Common/Analyzer.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
-template<typename T, typename A>
-class Analyzer
+template<typename T>
+concept CounterConcept = requires(T t) 
+{
+	T::readings;
+}; 
+
+template<typename T, typename A> class Analyzer {
+public:
+	static decltype(auto) Get()
+	{
+		std::cout<<"\nT"<<std::endl;
+	};
+};
+//template<typename ConfigT> class Counter;
+template<CounterConcept C, typename A>
+class Analyzer<C,A>
 {
 public:
 	static decltype(auto) Get()
-	{auto b = T::readings->Begin();
+	{
+		std::cout<<"Counter"<<std::endl;
 	};
 };
 
--- Home/Counter.hpp
+++ Home/Counter.hpp
@@ -24,7 +24,7 @@
 
 #pragma once
 
-template<typename T, typename A> class Analyzer;
+//~ template<typename T, typename A> class Analyzer;
 
 template<typename ConfigT>
 class Counter
@@ -128,7 +128,7 @@ public:
 	};
 	bool Update(InputIterator begin, InputIterator end) { Logger::Log("Update in",*(begin), *(++begin)); return true; }
 private:
-	template<typename T, typename A> friend class Analyzer;
+	//~ template<typename T, typename A> friend class Analyzer;
 	void write(const std::string sourcePath = ".")
 	{
 		Logger::Log<Info>()<<"Write Counter: "<<GetName()<<" to: "<<csv->GetDestinationPath()<<std::endl;
