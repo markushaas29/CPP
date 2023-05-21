c7b0b15dbe65494f8095fbb32cbe2666d22f79cc
markus <markus@gmx.de>
2023-05-21 16:58:42 +0200

    LitB

--- Is/Is.hpp
+++ Is/Is.hpp
@@ -13,7 +13,9 @@ class CompileTime
 	template<typename T, Literal L, bool B> friend class Is;
 	constexpr static void exec(const char* m)	{	; }
 };
+
 class Ignore {};
+
 class Asserting 
 {
 	template<typename T, Literal L, bool B> friend class Is;
@@ -23,6 +25,7 @@ class Asserting
 		assert(false); 
 	}
 };
+
 class Throwing 
 {
 	template<typename T, Literal L, bool B> friend class Is;
@@ -32,11 +35,13 @@ class Throwing
 		throw m; 
 	}
 };
+
 class Terminating 
 {
 	template<typename T, Literal L, bool B> friend class Is;
 	static void exec(const std::string& m)	{	std::terminate(); }
 };
+
 class Logging 
 {
 	template<typename T, Literal L, bool B> friend class Is;
@@ -48,15 +53,13 @@ template<typename D>
 class IsBase 
 {
 public:
-//	bool operator()(bool c)
-//	{
-//		return c;
-//	}
 protected:
-	constexpr IsBase(const char* m): message{m} {}
-	const std::string ToString() { return std::string{message}; };
-	constexpr const char* c() { return message; };
+	constexpr IsBase(const char* m, const char* l): message{m}, literal{l} {}
+	const std::string ToString() { return std::string(literal) + " > " + std::string{message}; };
+	constexpr const char* mes() { return message; };
+	constexpr const char* lit() { return literal; };
 private:
+	const char* literal;
 	const char*  message;
 };
 
@@ -66,15 +69,14 @@ class Is: public IsBase<Is<T,L,B>>
 	using Policy = T; 
 	using Base = IsBase<Is<T,L,B>>;
 public:
-	Is(const std::string& m = ""): Base{m.c_str()} {}
+	Is(const std::string& m = ""): Base{m.c_str(), L.Value()} {}
 	bool operator()(bool c)
 	{
 		if(!c)
-			Policy::exec( literal + Base::ToString());
+			Policy::exec( Base::ToString());
 		return c;
 	}
 private:
-	const std::string literal{L.Value()};
 };
 
 template<Literal L, bool B>
@@ -82,7 +84,7 @@ class Is<CompileTime, L,B>: public IsBase<Is<CompileTime,L,B>>
 {
 	using Base = IsBase<Is<CompileTime,L,B>>;
 public:
-	constexpr Is(const char* m = ""): Base{m} {}
+	constexpr Is(const char* m = ""): Base{m, L.Value()} {}
 	constexpr bool operator()(bool con)
 	{
 		static_assert(B);
