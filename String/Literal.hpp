5402e8ae206691f73c3f0c51e114185c037a93bd
markus <markus@gmx.de>
2023-05-27 17:26:34 +0200

    Ref

--- String/Literal.hpp
+++ String/Literal.hpp
@@ -7,28 +7,9 @@
 #pragma once
 
 template< auto N>
-constexpr decltype(auto) ToArray(const char(&s)[N])
-{
-	std::array<char,N> value;
-	for(int i= 0; i<N; ++i)
-		value[i] = s[i];
-
-	return value;
-}
-
-constexpr std::size_t Len(const char * a)
-	{
-		const char* end = a;
-		while(*end) ++end;
-		return end - a;
-	}
-
-template< auto N>
 struct Literal 
 {
-	constexpr Literal(const char(&s)[N]): Value{ToArray(s)}
-	{
-	}
+	constexpr Literal(const char(&s)[N]): Value{ToArray(s)}	{	}
 	static constexpr std::size_t Size = N;
 	const std::array<char,N> Value;
 	constexpr decltype(auto) Ptr() const { return Value.data(); };
--- String/LiteralTest/Test.cpp
+++ String/LiteralTest/Test.cpp
@@ -9,11 +9,11 @@ int main()
 	constexpr Literal L{"Main"};
 	constexpr const char C[] ="Main2";
 	constexpr Literal L2{C};
+	
 	std::cout<<L<<std::endl;
 	std::cout<<L2<<std::endl;
 	std::cout<<L.Ptr()<<std::endl;
-
-	const auto P = L.Ptr();
+	std::cout<<L2.Ptr()<<std::endl;
 
 	std::cout<<"END"<<std::endl;
 
