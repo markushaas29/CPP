2a9160ce5bcf758e74fcbded96f2fd7580133a6d
markus <markus@gmx.de>
2023-06-08 15:22:07 +0200

    Base

--- Matrix/Matrix_Slice.hpp
+++ Matrix/Matrix_Slice.hpp
@@ -10,19 +10,47 @@ template<typename... Args>
 constexpr bool All(bool b, Args... a) { return b && All(a...); }
 
 template<size_t N>
-class MatrixSlice
+class MatrixDescriptorBase
 {
 public:
-	MatrixSlice() = default;
-	MatrixSlice(std::size_t o, std::initializer_list<size_t> e);
-	MatrixSlice(std::size_t o, std::initializer_list<size_t> e, std::initializer_list<size_t> s);
-	MatrixSlice(auto... dims)
+	MatrixDescriptorBase() = default;
+	MatrixDescriptorBase(std::size_t o, std::initializer_list<size_t> e);
+	MatrixDescriptorBase(std::size_t o, std::initializer_list<size_t> e, std::initializer_list<size_t> s);
+	MatrixDescriptorBase(auto... dims)
 	{
-          //IsT<Throwing>("Dim != N")(Dim==N);
        std::initializer_list<int> il({dims...} );
  	   std::copy (il.begin(), il.end(), extents.begin());
 	};
-	MatrixSlice(std::array<std::size_t,N> e, std::array<std::size_t,N> s): extents{e}, strides{s}, size{e[0] * s[0]} {};
+	MatrixDescriptorBase(std::array<std::size_t,N> e, std::array<std::size_t,N> s): extents{e}, strides{s}, size{e[0] * s[0]} {
+
+	   std::cout<<"Base "<<extents[0]<<std::endl;
+	};
+	template<typename... Dims, typename std::enable_if<All(std::is_convertible<Dims...,std::size_t>::value),void>::type>
+	std::size_t operator()(Dims... dims) const
+	{
+		std::size_t args[N] { size_t(dims)... };
+		return start+std::inner_product(args,args+N,strides.begin(),size_t{0});
+	}
+	
+private:
+	std::size_t size;
+	std::size_t start;
+	std::array<std::size_t,N> extents;
+	std::array<std::size_t,N> strides;
+	friend std::ostream& operator<<(std::ostream& s, const MatrixDescriptorBase& i) { return s<<"Size: "<<i.size<<"\tStart: "<<i.start;  }
+};
+template<size_t N>
+class MatrixSlice: public MatrixDescriptorBase<N>
+{
+	using Base = MatrixDescriptorBase<N>;
+public:
+	MatrixSlice() = default;
+	MatrixSlice(auto... dims): Base(dims...)
+	{
+       std::initializer_list<int> il({dims...} );
+ 	   std::copy (il.begin(), il.end(), extents.begin());
+	};
+	MatrixSlice(std::array<std::size_t,N> e, std::array<std::size_t,N> s): Base{e,s}, extents{e}, strides{s}, size{e[0] * s[0]} {};
 	template<typename... Dims, typename std::enable_if<All(std::is_convertible<Dims...,std::size_t>::value),void>::type>
 	std::size_t operator()(Dims... dims) const
 	{
@@ -39,19 +67,17 @@ private:
 };
 
 template<>
-class MatrixSlice<2>
+class MatrixSlice<2>: public MatrixDescriptorBase<2>
 {
+	using Base = MatrixDescriptorBase<2>;
 public:
 	MatrixSlice() = default;
-	MatrixSlice(std::size_t o, std::initializer_list<size_t> e);
-	MatrixSlice(std::size_t o, std::initializer_list<size_t> e, std::initializer_list<size_t> s);
-	template<typename... Dims>	MatrixSlice(Dims... dims)
+	MatrixSlice(auto... dims): Base(dims...)
 	{
-          //IsT<Throwing>("Dim != N")(Dim==N);
        std::initializer_list<int> il({dims...} );
  	   std::copy (il.begin(), il.end(), extents.begin());
 	};
-	MatrixSlice(std::array<std::size_t,2> e, std::array<std::size_t,2> s): extents{e}, strides{s}, size{e[0] * s[0]} {};
+	MatrixSlice(std::array<std::size_t,2> e, std::array<std::size_t,2> s): Base{e,s}, extents{e}, strides{s}, size{e[0] * s[0]} {};
 
 	template<typename... Dims, typename std::enable_if<All(std::is_convertible<Dims...,std::size_t>::value),void>::type>
 	std::size_t operator()(size_t i, size_t j) const	{	return start+i*strides[0]+j;	}
@@ -65,13 +91,12 @@ private:
 };
 
 template<>
-class MatrixSlice<1>
+class MatrixSlice<1>: public MatrixDescriptorBase<1>
 {
+	using Base = MatrixDescriptorBase<1>;
 public:
 	MatrixSlice() = default;
-	MatrixSlice(std::size_t o, std::initializer_list<size_t> e);
-	MatrixSlice(std::size_t o, std::initializer_list<size_t> e, std::initializer_list<size_t> s);
-	MatrixSlice(std::array<std::size_t,1> e, std::array<std::size_t,1> s): extents{e}, strides{s}, size{e[0] * s[0]} {};
+	MatrixSlice(std::array<std::size_t,1> e, std::array<std::size_t,1> s): Base{e,s},extents{e}, strides{s}, size{e[0] * s[0]} {};
 	
 	template<typename... Dims, typename std::enable_if<All(std::is_convertible<Dims...,std::size_t>::value),void>::type>
 	std::size_t operator()(size_t o) const	{	return o;	}
