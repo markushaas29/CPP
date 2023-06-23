c223eee4c6f79ca8fd52bbd9744e9c27147fc5cc
markus <markus@gmx.de>
2023-06-23 15:30:30 +0200

    Rename

---
+++ Matrix/MatrixElemenTest/Test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cassert> 
+#include <vector> 
+#include <tuple> 
+#include <memory> 
+#include "../MatrixCell.hpp"
+#include "../../Common/ShortNames.hpp"
+using namespace ShortNames;
+
+int main()
+{
+	std::cout<<"START"<<std::endl;
+
+	assert(false);
+	
+	std::cout<<"END"<<std::endl;
+//
+    return 0;
+}
