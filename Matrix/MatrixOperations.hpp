4b19d343264957bb94b9ccfef61225ee6e96b78f
markus <markus@gmx.de>
2023-07-15 17:25:20 +0200

    Special

--- Matrix/MatrixOperations.hpp
+++ Matrix/MatrixOperations.hpp
@@ -27,6 +27,12 @@ struct Sub
 	using Type = decltype(Subtraction::Calculate(std::declval<L>(), std::declval<R>()));
 };
 
+template<typename L, typename F>
+struct Func
+{
+	using Type = decltype(Subtraction::Calculate(std::declval<L>(), std::declval<F>()));
+};
+
 template<template<typename,typename> class Op, typename M1, typename M2>
 class MatrixOperation 
 {
@@ -34,6 +40,25 @@ public:
 	using Left = M1;
 	using Right = std::remove_reference<M2>::type;
 	using ExpressionType = Op<typename Left::InputType, typename Right::InputType>::Type;
+	using DataType = std::shared_ptr<ExpressionType>;    
+	using DescriptorType = MatrixDescriptor<Left::Order,ExpressionType,ExpressionType>;
+	using MatrixType = Matrix<Left::Order,DescriptorType>;
+	inline static constexpr const char TypeIdentifier[] = "MatrixOperation";
+    inline static constexpr Literal LiteralType{TypeIdentifier};
+	static int constexpr Size = Left::Order;
+protected:
+private:
+	template<typename U> using IsT =  Is<U,LiteralType>;
+	friend std::ostream& operator<<(std::ostream& s, const MatrixOperation& me) { return s;  }
+};
+
+template<typename M1, typename M2>
+class MatrixOperation<Func,M1,M2> 
+{
+public:
+	using Left = M1;
+	using Right = std::remove_reference<M2>::type;
+	using ExpressionType = Func<typename Left::InputType, M2>::Type;
 	using DataType = std::shared_ptr<ExpressionType>;    
 	using DescriptorType = MatrixDescriptor<Left::Order,ExpressionType,ExpressionType>;
 	using MatrixType = Matrix<Left::Order,DescriptorType>;
