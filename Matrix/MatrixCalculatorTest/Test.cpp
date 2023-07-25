6f5d13960bfe35e04a2d9acb8147f5df275872ad
markus <markus@gmx.de>
2023-07-25 13:25:07 +0200

    CompileCalc

--- Matrix/MatrixCalculatorTest/Test.cpp
+++ Matrix/MatrixCalculatorTest/Test.cpp
@@ -37,79 +37,79 @@ int main()
 
 	auto aA = m44.Apply([&](const auto& e1){ return *e1 + 10; });
 	std::cout<<"Matrix mA "<<aA<<"\n";
-	auto m44m1 = m44*m1;
-	std::cout<<"Matrix m44*m1 "<<m44m1<<"\n";
-	assert(*m44m1(0)==30);
-	//assert(m44m1.ExpRow(0)==30);
-	auto a5 = m44 + 5;
-	assert(*a5(0,3)==9);
-	std::cout<<"Matrix m2(0.3) "<<*(a5.ExpRow(0)[3])<<"\n";
-
-	auto s5 = m44 - 5;
-	assert(*s5(0,3)==-1);
-	std::cout<<"Matrix m2(0.3) "<<*(s5.ExpRow(0)[3])<<"\n";
-	
-	auto m5 = m44 * 5;
-	assert(*m5(0,3)==20);
-	std::cout<<"Matrix m2(0.3) "<<*(m5.ExpRow(0)[3])<<"\n";
-	
-	auto d2 = m44  / 2;
-	assert(*d2(0,3)==2);
-	std::cout<<"Matrix m2(0.3) "<<*(d2.ExpRow(0)[3])<<"\n";
-	
-	
-
-	M1D m1d{
-		{1.1,2.2,3.3,4.4}	
-	};
-
-
-
-	std::cout<<"Matrix m1 * m1 \n"<<(m1d * m1d )<<"\n";
-	std::cout<<"Matrix m2 + m2 \n"<<(m44 + m44 )<<"\n";
-	std::cout<<"Matrix m2 + m2 \n"<<(a5 - m44 )<<"\n";
-	std::cout<<"Matrix d2\n"<<d2<<"\n";
-	auto m2SA = a5 - m44 + d2 ;
-	std::cout<<"Matrix m25A \n"<<*m2SA(0,3)<<"\n";
-	assert(*m2SA(0,3)==7);
-	std::cout<<"Matrix m2 + m2 \n"<<m2SA<<"\n";
-	
-	M2D m2d {
-		{1.1, 2.2, 3.3, 4.4},
-		{1.1, 2.2, 3.3, 4.4},
-		{1.1, 2.2, 3.3, 4.4},
-		{1.1, 2.2, 3.3, 4.4}
-	};
-	std::cout<<"Matrix m2 EL(0.3) "<<(d2.ElAt(0,3))<<"\n\n";
-	std::cout<<"Matrix m2 EL(0.3) "<<(d2.ElAt(0,3))<<"\n\n";
-//	std::cout<<"Matrix m3 "<<(m2d * m2d )<<"\n";
-//	std::cout<<"Matrix m3 "<<(m2d * m2d * m1d)<<"\n";
-//	//std::cout<<"Matrix m3 "<<(m1d * m2d)<<"\n";
+//	auto m44m1 = m44*m1;
+//	std::cout<<"Matrix m44*m1 "<<m44m1<<"\n";
+//	assert(*m44m1(0)==30);
+//	//assert(m44m1.ExpRow(0)==30);
+//	auto a5 = m44 + 5;
+//	assert(*a5(0,3)==9);
+//	std::cout<<"Matrix m2(0.3) "<<*(a5.ExpRow(0)[3])<<"\n";
 //
-	M2 m35 {
-		{1, 2, 3, 4, 5},
-		{6, 7, 8, 9,10},
-		{11, 12, 13, 14, 15},
-	};
-//	std::cout<<"Matrix m3 "<<(m35 )<<"\n";
-////	std::cout<<"Matrix m3 "<<((m35 + m35)+(m35 + m35))<<"\n";
-//	//std::cout<<"Matrix m3 "<<((m35 + m35)+(m35 + m35) + m35)<<"\n";
-//	std::cout<<"Matrix m3 "<<(m35+m35 + m35 )<<"\n";
-//	auto mad = m35 + m35 -m35 +m35; 
-	auto mop = ((m35 * 2)- m35 - (m35 + 5) + (m35 - 6) -m35 +m35 );
-	std::cout<<"Matrix m3 Sub"<<mop<<"\n";
-	assert(*mop(0,0)==-10);
-	assert((double)(*(mop.ExpRow(0)[0]))==-10);
-	std::cout<<"Matrix m3 Sub 00"<<*mop(0,0)<<"\n";
-	std::cout<<"Matrix m3 Sub 00"<<(*(mop.ExpRow(0)[0]))<<"\n";
-	std::cout<<"Matrix m3 Sub 00"<<(double)(*(mop.ExpRow(0)[0]))<<"\n";
-	
-//	auto m35m1 = m35 * m1;
-//	std::cout<<"Matrix m35m1 "<<m35m1<<"\n";
-	std::cout<<"Matrix m3 + 2"<<(m35 + 2)<<"\n";
-//	std::cout<<"Matrix m3 "<<(m35 - 2)<<"\n";
-//	std::cout<<"Matrix m3 "<<(m35 * 2)<<"\n";
-//	std::cout<<"Matrix m3 "<<(m35 / 2)<<"\n";
+//	auto s5 = m44 - 5;
+//	assert(*s5(0,3)==-1);
+//	std::cout<<"Matrix m2(0.3) "<<*(s5.ExpRow(0)[3])<<"\n";
+//	
+//	auto m5 = m44 * 5;
+//	assert(*m5(0,3)==20);
+//	std::cout<<"Matrix m2(0.3) "<<*(m5.ExpRow(0)[3])<<"\n";
+//	
+//	auto d2 = m44  / 2;
+//	assert(*d2(0,3)==2);
+//	std::cout<<"Matrix m2(0.3) "<<*(d2.ExpRow(0)[3])<<"\n";
+//	
+//	
+//
+//	M1D m1d{
+//		{1.1,2.2,3.3,4.4}	
+//	};
+//
+//
+//
+//	std::cout<<"Matrix m1 * m1 \n"<<(m1d * m1d )<<"\n";
+//	std::cout<<"Matrix m2 + m2 \n"<<(m44 + m44 )<<"\n";
+//	std::cout<<"Matrix m2 + m2 \n"<<(a5 - m44 )<<"\n";
+//	std::cout<<"Matrix d2\n"<<d2<<"\n";
+//	auto m2SA = a5 - m44 + d2 ;
+//	std::cout<<"Matrix m25A \n"<<*m2SA(0,3)<<"\n";
+//	assert(*m2SA(0,3)==7);
+//	std::cout<<"Matrix m2 + m2 \n"<<m2SA<<"\n";
+//	
+//	M2D m2d {
+//		{1.1, 2.2, 3.3, 4.4},
+//		{1.1, 2.2, 3.3, 4.4},
+//		{1.1, 2.2, 3.3, 4.4},
+//		{1.1, 2.2, 3.3, 4.4}
+//	};
+//	std::cout<<"Matrix m2 EL(0.3) "<<(d2.ElAt(0,3))<<"\n\n";
+//	std::cout<<"Matrix m2 EL(0.3) "<<(d2.ElAt(0,3))<<"\n\n";
+////	std::cout<<"Matrix m3 "<<(m2d * m2d )<<"\n";
+////	std::cout<<"Matrix m3 "<<(m2d * m2d * m1d)<<"\n";
+////	//std::cout<<"Matrix m3 "<<(m1d * m2d)<<"\n";
+////
+//	M2 m35 {
+//		{1, 2, 3, 4, 5},
+//		{6, 7, 8, 9,10},
+//		{11, 12, 13, 14, 15},
+//	};
+////	std::cout<<"Matrix m3 "<<(m35 )<<"\n";
+//////	std::cout<<"Matrix m3 "<<((m35 + m35)+(m35 + m35))<<"\n";
+////	//std::cout<<"Matrix m3 "<<((m35 + m35)+(m35 + m35) + m35)<<"\n";
+////	std::cout<<"Matrix m3 "<<(m35+m35 + m35 )<<"\n";
+////	auto mad = m35 + m35 -m35 +m35; 
+//	auto mop = ((m35 * 2)- m35 - (m35 + 5) + (m35 - 6) -m35 +m35 );
+//	std::cout<<"Matrix m3 Sub"<<mop<<"\n";
+//	assert(*mop(0,0)==-10);
+//	assert((double)(*(mop.ExpRow(0)[0]))==-10);
+//	std::cout<<"Matrix m3 Sub 00"<<*mop(0,0)<<"\n";
+//	std::cout<<"Matrix m3 Sub 00"<<(*(mop.ExpRow(0)[0]))<<"\n";
+//	std::cout<<"Matrix m3 Sub 00"<<(double)(*(mop.ExpRow(0)[0]))<<"\n";
+//	
+////	auto m35m1 = m35 * m1;
+////	std::cout<<"Matrix m35m1 "<<m35m1<<"\n";
+//	std::cout<<"Matrix m3 + 2"<<(m35 + 2)<<"\n";
+////	std::cout<<"Matrix m3 "<<(m35 - 2)<<"\n";
+////	std::cout<<"Matrix m3 "<<(m35 * 2)<<"\n";
+////	std::cout<<"Matrix m3 "<<(m35 / 2)<<"\n";
 
 	M3 m3 {
 		{
