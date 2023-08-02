34447fd33bd6cae02c5cfc74c91c2f55c59036e8
markus <markus@gmx.de>
2023-08-02 17:31:11 +0200

    IoR

--- Matrix/MatrixIO.hpp
+++ Matrix/MatrixIO.hpp
@@ -12,6 +12,7 @@ class MatrixIO: IOut
 public:
 	MatrixIO() {}
     virtual std::unique_ptr<std::ofstream> operator()(std::unique_ptr<std::ofstream> of) { return of; };
+	virtual std::ostream& operator()(std::ostream& s) {	return s;	}
     virtual std::ostream& operator()(std::ostream& s, const M* m) 
 	{ 
 		if constexpr (M::Order==1)
@@ -26,22 +27,6 @@ public:
 		}
 		return s;
 	};
-	virtual std::ostream& operator()(std::ostream& s) 
-	{
-//		if constexpr (M::Order==1)
-//		{
-//			std::for_each(m->elements->cbegin(), m->elements->cend(), [&](auto& v) { s<<*v<<", "; });
-//			s<<"\nExpressions:\n";
-//			std::for_each(m->expressions->cbegin(), m->expressions->cend(), [&](auto& v) { s<<*v<<", "; });
-//			return s;
-//		}
-//		else
-//		{
-//			for(auto i = 0; i != m->Rows(); ++i)
-//				s<<"{"<<(*m)[i]<<"}"<<std::endl;
-//		}
-		return s;
-	}
 private:
 	friend M;
 };
