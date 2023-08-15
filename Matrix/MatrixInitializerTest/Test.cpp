f2c3815b5a0c6100ced589f95dc983a652c45589
markus <markus@gmx.de>
2023-08-15 17:48:14 +0200

    TSem

--- Matrix/MatrixInitializerTest/Test.cpp
+++ Matrix/MatrixInitializerTest/Test.cpp
@@ -76,11 +76,13 @@ class MatrixInitializerTest
 		
 		  	std::cout<<"MI2: "<<mi2.Get()<<std::endl;
 			auto path = std::string{ "/home/markus/Downloads/CSV_TestFiles_2/D.mat" };
+			auto sem = std::string{ "/home/markus/Downloads/CSV_TestFiles_2/Sem.mat" };
 			auto spath = std::string{ "/home/markus/Downloads/CSV_TestFiles_2/S.csv" };
 			auto ipath = std::string{ "/home/markus/Downloads/CSV_TestFiles_2/I.mat" };
 			auto uipath = std::string{ "/home/markus/Downloads/CSV_TestFiles_2/UI.mat" };
 			auto msr = MatrixReader(path);
 			auto mssr = MatrixReader(spath);
+			auto msem = MatrixReader<double>(sem);
 			auto mdr = MatrixReader<double>(path);
 			auto muir = MatrixReader<uint>(uipath);
 			std::cout<<"MI: "<<mdr.M<2>()<<std::endl;
@@ -88,6 +90,7 @@ class MatrixInitializerTest
 			
 			auto mir = MatrixReader<int>(ipath);
 			std::cout<<"MI: "<<mir.M<1>()<<std::endl;
+			std::cout<<"Sem: "<<msem.M<2>()<<std::endl;
 			assert(mir.IsDim<1>());
 			std::cout<<"MS"<<mssr.M<2>()<<std::endl;
 		
