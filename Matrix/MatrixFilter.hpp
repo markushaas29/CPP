0f49eb2bafa5a200aafdc05e5040a1f48a4b80e2
markus <markus@gmx.de>
2023-08-19 15:51:59 +0200

    CopyArr

--- Matrix/MatrixFilter.hpp
+++ Matrix/MatrixFilter.hpp
@@ -30,6 +30,25 @@ public:
         {
     	    typename MatrixType::IsT<Throwing>(Format("Index: ",i ," exceeds extents!"))(i<matrix.Cols());
     	    std::vector<typename MatrixType::DataType> result;
+    	    std::array<size_t,MatrixType::Order> e = copy(matrix.descriptor.Extents());
+
+    	    for(int i = 0; i < matrix.Rows(); ++i)
+    	    {
+    	        auto row = matrix.row(i);
+				if(pred(*row[i]))
+    	        	std::for_each(row.begin(), row.end(), [&](auto e){ result.push_back(e); });
+    	    }
+    	    
+    	    e[0] = result.size() / matrix.Cols();
+    	    
+			return MatrixType(typename MatrixType::DescriptorType{e,copy(matrix.descriptor.Strides())}, result);
+        }
+	}
+	decltype(auto) operator()(std::function<bool(const typename MatrixType::ElementType& i)> pred = [](const typename MatrixType::ElementType& e) { return e==0; }) const 
+	{
+		if constexpr (MatrixType::Order==2)
+        {
+    	    std::vector<typename MatrixType::DataType> result;
     	    std::array<size_t,MatrixType::Order> e;
     	    std::array<size_t,MatrixType::Order> s;
     	    std::copy(matrix.descriptor.Extents().begin(), matrix.descriptor.Extents().end(), e.begin());
@@ -50,4 +69,12 @@ private:
 	MatrixType matrix;
 	template<typename U> using IsT =  Is<U,LiteralType>;
 	friend std::ostream& operator<<(std::ostream& s, const MatrixFilter& me) { return s;  }
+
+	template<size_t N>
+	decltype(auto) copy(std::array<size_t,N> arr) const
+	{
+    	std::array<size_t,MatrixType::Order> res;
+		std::copy(arr.begin(), arr.end(), res.begin());
+		return res;
+	}
 };
