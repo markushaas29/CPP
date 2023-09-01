606dfc0062a762458a452663ace6b22f73da4d6c
markus <markus@gmx.de>
2023-09-01 13:39:31 +0200

    sub

--- Functional/Functional.hpp
+++ Functional/Functional.hpp
@@ -146,3 +146,12 @@ constexpr decltype(auto) operator+(const BinaryFunctional<D1,L1,R1>& f1, const U
 
 template<template<typename,typename> class D1,typename L1, typename R1, template<typename> class D2,typename V2>
 constexpr decltype(auto) operator+(const UnaryFunctional<D2,V2>& f1, const BinaryFunctional<D1,L1,R1>& f2) { return Func<Add>(f1,f2); }
+
+template<template<typename,typename> class D1,typename L1, typename R1, template<typename,typename> class D2,typename L2, typename R2>
+constexpr decltype(auto) operator-(const BinaryFunctional<D1,L1,R1>& f1, const BinaryFunctional<D2,L2,R2>& f2) { return Func<Sub>(f1,f2); }
+
+template<template<typename,typename> class D1,typename L1, typename R1, template<typename> class D2,typename V2>
+constexpr decltype(auto) operator-(const BinaryFunctional<D1,L1,R1>& f1, const UnaryFunctional<D2,V2>& f2) { return Func<Sub>(f1,f2); }
+
+template<template<typename,typename> class D1,typename L1, typename R1, template<typename> class D2,typename V2>
+constexpr decltype(auto) operator-(const UnaryFunctional<D2,V2>& f1, const BinaryFunctional<D1,L1,R1>& f2) { return Func<Sub>(f1,f2); }
--- Functional/Test.cpp
+++ Functional/Test.cpp
@@ -62,8 +62,13 @@ class FunctionalTest
 			auto ap = ac + ac;
 			auto i = ap();
 			std::cout<<"Operator "<<ap<<std::endl;
+			std::cout<<"Operator "<<(ac + ac)<<std::endl;
 			std::cout<<"Operator "<<(ac + cm)<<std::endl;
 			std::cout<<"Operator "<<(cm + ac)<<std::endl;
+
+			std::cout<<"Operator "<<(ac - ac)<<std::endl;
+			std::cout<<"Operator "<<(ac - cm)<<std::endl;
+			std::cout<<"Operator "<<(cm - ac)<<std::endl;
 
 			std::cout<<"END"<<std::endl;
 		   
