a9307c276c1f3154173f30085e356937ea414c52
markus <markus@gmx.de>
2023-09-01 11:59:09 +0200

    Rename

--- Functional/Functional.hpp
+++ Functional/Functional.hpp
@@ -35,7 +35,7 @@ private:
 };
 
 template<template<typename,typename> class D,typename L, typename R>
-class OperationBase 
+class BinaryFunctional 
 {
 	using LeftType = L;
 	using RightType = R;
@@ -43,22 +43,22 @@ class OperationBase
 	inline static constexpr const char* sign = Derived::sign; 
 	friend class D<L,R>;
 public:
-	OperationBase(const LeftType& l,const RightType& r ): right{r}, left{l} {}
+	BinaryFunctional(const LeftType& l,const RightType& r ): right{r}, left{l} {}
 	decltype(auto) operator()(const auto& v) const { return Derived::op(left,right); }
 	decltype(auto) operator()() const { return Derived::op(left,right); }
 	template<typename T>
 	operator T() const { return static_cast<T>((*this)()); }
 private:
-	friend std::ostream& operator<<(std::ostream& s, const OperationBase& c) { return s<<"{"<<c.left<<" "<<c.sign<<" "<<c.right<<"}";  }
+	friend std::ostream& operator<<(std::ostream& s, const BinaryFunctional& c) { return s<<"{"<<c.left<<" "<<c.sign<<" "<<c.right<<"}";  }
 	RightType right;
 	LeftType left;
 };
 
 template<typename L, typename R>
-class Add: public OperationBase<Add,L,R>
+class Add: public BinaryFunctional<Add,L,R>
 {
-	using Base = OperationBase<Add,L,R>;
-	friend class OperationBase<Add,L,R>;
+	using Base = BinaryFunctional<Add,L,R>;
+	friend class BinaryFunctional<Add,L,R>;
 public:
 	Add(const L& l, const R& r): Base{l,r} {}
 private:
@@ -67,10 +67,10 @@ private:
 };
 
 template<typename L, typename R>
-class Sub: public OperationBase<Sub,L,R>
+class Sub: public BinaryFunctional<Sub,L,R>
 {
-	using Base = OperationBase<Sub,L,R>;
-	friend class OperationBase<Sub,L,R>;
+	using Base = BinaryFunctional<Sub,L,R>;
+	friend class BinaryFunctional<Sub,L,R>;
 public:
 	Sub(const L& l, const R& r): Base{l,r} {}
 private:
@@ -79,10 +79,10 @@ private:
 };
 
 template<typename L, typename R>
-class Mul: public OperationBase<Mul,L,R>
+class Mul: public BinaryFunctional<Mul,L,R>
 {
-	using Base = OperationBase<Mul,L,R>;
-	friend class OperationBase<Mul,L,R>;
+	using Base = BinaryFunctional<Mul,L,R>;
+	friend class BinaryFunctional<Mul,L,R>;
 public:
 	Mul(const L& l, const R& r): Base{l,r} {}
 private:
@@ -91,10 +91,10 @@ private:
 };
 
 template<typename L, typename R>
-class Div: public OperationBase<Div,L,R>
+class Div: public BinaryFunctional<Div,L,R>
 {
-	using Base = OperationBase<Div,L,R>;
-	friend class OperationBase<Div,L,R>;
+	using Base = BinaryFunctional<Div,L,R>;
+	friend class BinaryFunctional<Div,L,R>;
 public:
 	Div(const L& l, const R& r): Base{l,r} {}
 private:
