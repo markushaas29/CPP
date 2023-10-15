15fc01f26e40806b4a69b5a500f52ea4b53ed51a
markus <markus@gmx.de>
2023-10-15 12:48:48 +0200

    xBase

--- Matrix/MatrixCategory.hpp
+++ Matrix/MatrixCategory.hpp
@@ -36,33 +36,43 @@ public:
 };
 
 template<typename T>
-class EquivalenceCat : public IMatrixCategory<T>
+class CategoryBase : public IMatrixCategory<T>
 {
 	using Base = IMatrixCategory<T>;
 public:
 	inline static constexpr const char TypeIdentifier[] = "Equivalence";
     inline static constexpr Literal TypeId{TypeIdentifier};
-
-	EquivalenceCat(typename Base::ElementType e): element(e) {}
-	virtual bool operator()(const Base::ElementType& e) const { return e==element; };
-private:
+protected:
+	CategoryBase(typename Base::ElementType e): element(e) {}
 	Base::ElementType element;
-	template<typename U> using IsT =  Is<U,TypeId>;
+private:
 	virtual std::ostream& display(std::ostream& s) const { return s<<TypeId<<": "<<element; }
 };
 
 template<typename T>
-class ContainCat : public IMatrixCategory<T>
+class EquivalenceCat : public CategoryBase<T>
 {
-	using Base = IMatrixCategory<T>;
+	using Base = CategoryBase<T>;
+public:
+	inline static constexpr const char TypeIdentifier[] = "Equivalence";
+    inline static constexpr Literal TypeId{TypeIdentifier};
+
+	EquivalenceCat(typename Base::ElementType e): Base(e) {}
+	virtual bool operator()(const Base::ElementType& e) const { return e==Base::element; };
+private:
+	template<typename U> using IsT =  Is<U,TypeId>;
+};
+
+template<typename T>
+class ContainCat : public CategoryBase<T>
+{
+	using Base = CategoryBase<T>;
 public:
 	inline static constexpr const char TypeIdentifier[] = "Contain";
     inline static constexpr Literal TypeId{TypeIdentifier};
 
-	ContainCat(typename Base::ElementType e): element(e) {}
-	virtual bool operator()(const Base::ElementType& e) const { return String_::Contains(e,element); };
+	ContainCat(typename Base::ElementType e): Base(e) {}
+	virtual bool operator()(const Base::ElementType& e) const { return String_::Contains(e,Base::element); };
 private:
-	Base::ElementType element;
 	template<typename U> using IsT =  Is<U,TypeId>;
-	virtual std::ostream& display(std::ostream& s) const { return s<<TypeId<<": "<<element; };
 };
