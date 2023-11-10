d6d7ca9c3a9b2ce6f08337ef51054638c5175a76
markus <markus@gmx.de>
2023-11-10 14:36:50 +0100

    Headers

--- Matrix/MatrixFwd.hpp
+++ Matrix/MatrixFwd.hpp
@@ -1,4 +1,18 @@
-#include "MatrixHeaders.hpp"
+#include <vector>
+#include <initializer_list>
+#include <memory>
+#include "Matrix_Ref.hpp"
+#include "MatrixDescriptor.hpp"
+#include "MatrixInit.hpp"
+#include "Matrix_Impl.hpp"
+#include "MatrixElement.hpp"
+#include "MatrixQuery.hpp"
+#include "MatrixCalculator.hpp"
+#include "MatrixAccess.hpp"
+#include "MatrixIO.hpp"
+#include "IMatrixQuery.hpp"
+#include "../Is/Is.hpp"
+#include "../String/Literal.hpp"
 
 #pragma once
 
---
+++ Matrix/MatrixHeaders.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+#include <initializer_list>
+#include <memory>
+#include "Matrix_Ref.hpp"
+#include "MatrixDescriptor.hpp"
+#include "MatrixInit.hpp"
+#include "Matrix_Impl.hpp"
+#include "MatrixElement.hpp"
+#include "MatrixQuery.hpp"
+#include "MatrixCalculator.hpp"
+#include "MatrixAccess.hpp"
+#include "MatrixIO.hpp"
+#include "IMatrixQuery.hpp"
+#include "../Is/Is.hpp"
+#include "../String/Literal.hpp"
