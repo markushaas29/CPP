aa58136df71baf4d0671f873580f7210fbdfa614
markus <markus@gmx.de>
2023-11-12 06:24:05 +0100

    V

--- Visitor/Test.cpp
+++ Visitor/Test.cpp
@@ -5,11 +5,16 @@
 #include <memory> 
 #include "Visitor.hpp"
 #include "../CSV/Elements.hpp"
+#include "../Common/Date.hpp"
 #include "../Quantity/Quantity.hpp"
 
 int main()
 {
 	std::cout<<"START"<<std::endl;
+
+	ElementVisitor<Date> vd;
+	Date d1(29,9,1986);
+
 	std::cout<<"END"<<std::endl;
    
 	return 0;
--- Visitor/Visitor.hpp
+++ Visitor/Visitor.hpp
@@ -1,73 +1,74 @@
-#ifndef VISITOR_HPP
-#define VISITOR_HPP
+#pragma once 
+class Date;
+template<typename,typename, typename> class Quantity;
 
-	//~ namespace FS
-	//~ {
-		//~ class DirectoryInfo;
-		//~ class FileInfo;
-	//~ }
+class BaseVisitor
+{
+public:
+	virtual ~BaseVisitor(){}
+};
+
+template<class T, typename R = void>
+class Visitor
+{
+public:
+	using ReturnType = R;
+	virtual ReturnType Visit(T&) = 0;
+};
+
+template<class T, typename R = void>
+class ElementVisitor: public BaseVisitor, public Visitor<Date>
+{
+public:
+	virtual ReturnType Visit(int& q) { std::cout<<"I"<<std::endl; };
+	virtual ReturnType Visit(Date& q) { std::cout<<"Q"<<std::endl; };
+};
+
+template<class T, typename R = void>
+class ConstVisitor
+{
+public:
+	using ReturnType = R;
+	virtual ReturnType Visit(const T&) = 0;
+};
 	
-	class BaseVisitor
+template<typename R = void>
+class BaseVisitable
+{
+public:
+	using ReturnType = R;
+	virtual ~BaseVisitable(){}
+	virtual ReturnType Accept(BaseVisitor&) = 0;
+protected:
+	template<class T>
+	static ReturnType AcceptImpl(T& visited, BaseVisitor& visitor) 
 	{
-	public:
-		virtual ~BaseVisitor(){}
-	};
-	
-	template<class T, typename R = void>
-	class Visitor
-	{
-	public:
-		using ReturnType = R;
-		virtual ReturnType Visit(T&) = 0;
-	};
-	
-	template<class T, typename R = void>
-	class ConstVisitor
-	{
-	public:
-		using ReturnType = R;
-		virtual ReturnType Visit(const T&) = 0;
-	};
+		if(Visitor<T,R>* p = dynamic_cast<Visitor<T,R>*>(&visitor))
+			return p->Visit(visited);
 		
-	template<typename R = void>
-	class BaseVisitable
+		return ReturnType();
+	}		
+	
+	template<class T>
+	static ReturnType AcceptConstImpl(const T& visited, BaseVisitor& visitor) 
 	{
-	public:
-		using ReturnType = R;
-		virtual ~BaseVisitable(){}
-		virtual ReturnType Accept(BaseVisitor&) = 0;
-	protected:
-		template<class T>
-		static ReturnType AcceptImpl(T& visited, BaseVisitor& visitor) 
-		{
-			if(Visitor<T,R>* p = dynamic_cast<Visitor<T,R>*>(&visitor))
-				return p->Visit(visited);
-			
-			return ReturnType();
-		}		
+		//~ std::cout<<"Accept"<<std::endl;
 		
-		template<class T>
-		static ReturnType AcceptConstImpl(const T& visited, BaseVisitor& visitor) 
-		{
-			//~ std::cout<<"Accept"<<std::endl;
-			
-			//~ if(ConstVisitor<FS::DirectoryInfo,R>* p = dynamic_cast<ConstVisitor<FS::DirectoryInfo,R>*>(&visitor))
-				//~ std::cout<<"Accept Directory"<<std::endl;
-			
-			//~ if(ConstVisitor<FS::FileInfo,R>* p = dynamic_cast<ConstVisitor<FS::FileInfo,R>*>(&visitor))
-			//~ {
-				//~ std::cout<<"Accept File"<<std::endl;
-				//~ p->Visit(visited);
-			//~ }
-			
-			//~ if(ConstVisitor<T,R>* p = dynamic_cast<ConstVisitor<T,R>*>(&visitor))
-				//~ return p->Visit(visited);
-			
-			return ReturnType();
-		}		
-	};
+		//~ if(ConstVisitor<FS::DirectoryInfo,R>* p = dynamic_cast<ConstVisitor<FS::DirectoryInfo,R>*>(&visitor))
+			//~ std::cout<<"Accept Directory"<<std::endl;
+		
+		//~ if(ConstVisitor<FS::FileInfo,R>* p = dynamic_cast<ConstVisitor<FS::FileInfo,R>*>(&visitor))
+		//~ {
+			//~ std::cout<<"Accept File"<<std::endl;
+			//~ p->Visit(visited);
+		//~ }
+		
+		//~ if(ConstVisitor<T,R>* p = dynamic_cast<ConstVisitor<T,R>*>(&visitor))
+			//~ return p->Visit(visited);
+		
+		return ReturnType();
+	}		
+};
 	
 #define DEFINE_VISITABLE() virtual ReturnType Accept(BaseVisitor& visitor) { return AcceptImpl(*this, visitor); }
 #define DEFINE_CONSTVISITABLE() virtual ReturnType AcceptConst(BaseVisitor& visitor) const { return AcceptConstImpl(*this, visitor); }
-
-#endif
