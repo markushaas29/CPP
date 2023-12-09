eff1f8bf016f6419e16cda550fd635a488cb799d
markus <markus@gmx.de>
2023-12-09 17:49:32 +0100

    Ibane

---
+++ Visitor/PredicateVisitor.hpp
@@ -0,0 +1,37 @@
+#include "Visitor.hpp"
+#include "../CSV/Element.hpp"
+#include "../CSV/Elements.hpp"
+#include "../Common/Date.hpp"
+#include "../Common/UniqueCast.hpp"
+#include "../Unit/Unit.hpp"
+#include "../Quantity/Quantity.hpp"
+#include "../Quantity/QuantityRatio.hpp"
+
+#pragma once 
+class Date;
+class IBAN;
+class IElement;
+template<typename,typename, typename> class Quantity;
+
+class PredicateVisitor: public VariadicVisitor<bool, Quantity<Sum,Pure,double>, Date, IBAN>
+{
+	using ReturnType = bool;
+public:
+	PredicateVisitor(std::unique_ptr<IElement> v): value{std::move(v)} {}
+	virtual ReturnType Visit(Quantity<Sum,Pure,double>& q) { return visit(q); }
+	virtual ReturnType Visit(Date& d) { return visit(d); }
+	virtual ReturnType Visit(IBAN& i) { return visit(i); }
+private:
+	friend std::ostream& operator<<(std::ostream& s, const PredicateVisitor& t) 	{ return s<<"Value: "<<(*t.value);	}
+	template<typename T>
+	ReturnType visit(T& q) 
+	{
+		if(auto p = Cast::dynamic_unique_ptr<T>(std::move(value)))
+		{
+			 value = std::make_unique<T>(*p);
+             return q == *p;
+		}
+		return false; 
+	};
+	std::unique_ptr<IElement> value;
+};
