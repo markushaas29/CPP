87c4516e056369bb713f7ccbbbe37bf5a0ee1be4
markus <markus@gmx.de>
2023-12-16 14:49:05 +0100

    IExpression

--- Expression/ExpressionBase.hpp
+++ Expression/ExpressionBase.hpp
@@ -1,18 +1,26 @@
 #pragma once
 
+class IExpression
+{
+public:
+	virtual ~IExpression() = 0;
+	virtual bool operator()() const = 0;
+	virtual bool operator()(bool v) const = 0; 
+};
+
 template<typename D>
-class Expression
+class Expression: IExpression
 {
 	using Derived = D;
 	using Type = Expression<D>;
 public:
 	virtual ~Expression(){};
-	decltype(auto) operator()() const
+	virtual bool operator()() const
 	{ 
 		auto cderived = const_cast<Type&>(*this);
 		return  static_cast<Derived&>(cderived);
 	};
-	decltype(auto) operator()(const auto& v) const { return v; }
+	virtual bool operator()(bool v) const { return v; }
 private:
 	friend D;
 };
