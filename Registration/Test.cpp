a1d284da67f400c385f5d506f9f5a92549f2cd9b
markus <markus@gmx.de>
2023-12-22 09:49:45 +0100

    T

--- Registration/Test.cpp
+++ Registration/Test.cpp
@@ -6,6 +6,7 @@
 #include "Registration.hpp"
 #include "../Common/DateTimes.hpp"
 #include "../CSV/Elements.hpp"
+#include "../Visitor/PredicateVisitor.hpp"
 #include "../CSV/Token.hpp"
 #include "../Quantity/Quantity.hpp"
 #include "../ObjectFactory/Factory.hpp"
@@ -28,6 +29,10 @@ class TypeRegistrationTest
 			auto reg2 = Registration<Factory<IToken>,SumToken, IBANToken, DateToken, BICToken, EmptyToken, IDToken, WordToken>(&fmt2);
 			auto qp2 = fmt2("SumToken","100");
 			std::cout<<*qp2<<std::endl;
+
+			auto fme =std::make_shared<Factory<IElement>>();
+			auto pfs = std::make_shared<CompositeFactory<IPredicateVisitor, Factory<IElement>>>(fme);
+     		pfs->Register("EQ",[](std::unique_ptr<IElement> e) { return std::make_unique<EqualVisitor>(std::move(e)); });
 
 			std::cout<<"END TypeRegistration"<<std::endl;
 
