e8863ee12c9b1e1096676527677d909dede4d940
markus <markus@gmx.de>
2023-12-22 07:23:21 +0100

    Comp

--- Registration/Registration.hpp
+++ Registration/Registration.hpp
@@ -1,18 +1,36 @@
 #include <map>
 #include <chrono>
 #include <memory>
+#include <tuple>
 #include "../Is/Is.hpp"
 #include "../String/Literal.hpp"
 
 #pragma once
 
-template<typename... T>
+template<typename I, typename... T>
 class Registration
 {
+	using Tup = std::tuple<T...>;
 public:
 	inline static constexpr const char TypeIdentifier[] = "Registration";
 	inline static constexpr Literal TypeId{TypeIdentifier};
 
+	Registration() = delete;
+	Registration(I* i): interface(i)	{ reg<0>();	}
 private:
+	std::tuple<T...> t;
+	I* interface;
 	friend std::ostream& operator<<(std::ostream& s, const Registration& c){return s;}
+	 
+	template<size_t N>
+	void reg()
+	{
+		if constexpr (std::tuple_size<Tup>()==N)
+			return;
+		else
+		{	//
+			std::cout<<std::get<N>(t)<<std::endl;
+			interface->Register("Q",[](const std::string& s) { return std::make_unique<std::tuple_element_t<N,Tup>>(s); });
+	    reg<N+1>();}
+	}
 };
--- Registration/Test.cpp
+++ Registration/Test.cpp
@@ -3,7 +3,7 @@
 #include <vector> 
 #include <tuple> 
 #include <memory> 
-#include "TypeRegistration.hpp"
+#include "Registration.hpp"
 #include "../Common/DateTimes.hpp"
 #include "../CSV/Elements.hpp"
 #include "../Quantity/Quantity.hpp"
@@ -16,14 +16,13 @@ class TypeRegistrationTest
 		{
 			std::cout<<"START TypeRegistration"<<std::endl;
 
-			Registration<IBAN> reg;
 
 			Factory<IElement> fmt;
-    		fmt.Register("Q",[](const std::string& s) { return std::make_unique<Quantity<Sum>>(s); });
+			auto reg = Registration<Factory<IElement>,Quantity<Sum>, IBAN>(&fmt);
     		fmt.Register("IBAN",[](const std::string& s) { return std::make_unique<IBAN>(s); });
 
 			auto qp = fmt("Q","100");
-			std::cout<<*qp<<std::endl;
+			std::cout<<fmt<<std::endl;
 
 			std::cout<<"END TypeRegistration"<<std::endl;
 
