9b839dcfbe3ab1c802c97b027bbde94f63350b4b
markus <markus@gmx.de>
2023-12-24 06:50:10 +0100

    Ref

--- Matrix/MatrixVisitorTest/Test.cpp
+++ Matrix/MatrixVisitorTest/Test.cpp
@@ -28,68 +28,10 @@ class MatrixVisitorTest
 	public:
 		int Run()
 		{
+			std::cout<<"START MatrixVisitor"<<std::endl;
 		    using MDS2 = MatrixDescriptor<2,std::string>;
-		    using MDI2 = MatrixDescriptor<2,int>;
-		    using MDD2 = MatrixDescriptor<2,double>;
-		    using MDS1 = MatrixDescriptor<1,std::string>;
-		    using MDD1 = MatrixDescriptor<1,double>;
-		    using MS1 = Matrix<1,MDS1>;
-		    using MD1 = Matrix<1,MDD1>;
-		    using MD2 = Matrix<2,MDD2>;
 		    using MS2 = Matrix<2,MDS2>;
-		    using MI2 = Matrix<2,MDI2>;
 		
-			using QV = Quantity<Volume>;
-			using QM = Quantity<Mass>;
-			using QS = Quantity<Sum>;
-			using Q = Quantity<Sum>;
-			using QSC = Quantity<Scalar>;
-			using T = std::tuple<Q,QM>;
-			using T3 = std::tuple<Q,QM, QV>;
-			using T3_2 = std::tuple<QM, QV, Q>;
-			using TR = std::tuple<Year,Month,Day,Quantity<Volume>>;
-		
-			using MIQ2 = MatrixProjector<MI2,Q>;
-			using MSQ1 = MatrixProjector<MS1,Q>;
-			using MST1 = MatrixProjector<MS1,T>;
-			using MIT2 = MatrixProjector<MI2,T>;
-			using MIT3 = MatrixProjector<MI2,T3>;
-			using MST2 = MatrixProjector<MS2,T>;
-			using MSC2 = MatrixProjector<MS2,QSC>;
-			using MDR2 = MatrixProjector<MD2,TR>;
-			
-			using MDF2 = MatrixFilter<MD2>;
-			using MIF2 = MatrixFilter<MI2>;
-			using MSF2 = MatrixFilter<MS2>;
-			std::cout<<"START"<<std::endl;
-		
-			MS1 ms1{
-				{std::string("1"),std::string("2")} 
-		    };
-
-			MS2 m33s {
-	            {"1", "2" ,"3"},
-    	        {"4", "5", "6"},
-        	    {"7", "8", "9"},
-        	};
-			
-			MS2 m23s {
-    	        {"4", "5", "6"},
-        	    {"7", "8", "9"},
-        	};
-		
-		 	MI2 m35 {
-		        {1, 2, 3, 4, 5},
-		        {6, 7, 8, 9,10},
-		        {11, 12, 13, 14, 15},
-		    };
-		 	
-			MI2 m33 {
-		        {3, 4, 5},
-		        {8, 9,10},
-		        {13, 14, 15},
-		    };
-
 			auto u22 = std::string{ "/home/markus/Downloads/CSV_TestFiles_2/U_2022.csv" };
 			auto u23 = std::string{ "/home/markus/Downloads/CSV_TestFiles_2/U_2023.csv" };
 			auto m22r = MatrixReader(u22);
