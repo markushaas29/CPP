933d2e4f88301e985d010c4387b83e82150f528f
markus <markus@gmx.de>
2023-12-31 05:54:12 +0100

    IComposite

---
+++ Matrix/MatrixComposition.hpp
@@ -0,0 +1,66 @@
+#include <memory>
+#include <tuple>
+#include <vector>
+#include "MatrixQuery.hpp"
+#include "../Is/Is.hpp"
+#include "../String/Literal.hpp"
+#include "../Quantity/Quantity.hpp"
+#include "../CSV/Elements.hpp"    
+#include "../Common/DateTimes.hpp"
+#include "../Common/TupleHelper.hpp"
+#include "../Visitor/CollectorVisitor.hpp"
+
+#pragma once
+
+template<std::size_t, typename> class Matrix;
+
+template<typename T, typename Q>
+class IMatrixComposite
+{
+public:
+	using MatrixType = T;
+	using QuantityType = Q;
+	//using UnitType = U;
+	using ElementType = T::ElementType;
+	//using ResultType = StrategyResult<QuantityType,MatrixType,UnitType>;
+	virtual Q operator()(T& m) const = 0;
+	virtual std::string_view Name() const = 0;
+//	virtual const std::vector<UnitType> Units() const = 0;
+};
+
+template<typename T, typename F, typename Q = Quantity<Sum>>
+class MatrixComposition: public IMatrixComposite<T,Q>
+{
+protected:
+	using Base = IMatrixComposite<T,Q>;
+	using QueryType = MatrixQuery<T>;
+	using UnitType = FactoryUnit<std::string, std::vector<FactoryUnit<std::string, std::string>>>;
+	using PredicateFactory = std::shared_ptr<F>;
+	using VisitorFactory = std::shared_ptr<Factory<BaseVisitor>>;
+public:
+	inline static constexpr const char TypeIdentifier[] = "MatrixComposition";
+    inline static constexpr Literal TypeId{TypeIdentifier};
+
+	MatrixComposition(PredicateFactory f, VisitorFactory v, const std::string& n): name{n}, predicates{f}, visitors{v} {}
+	virtual Q operator()(Base::MatrixType& m) const
+	{
+
+		auto mP = m | (*predicates)("EqualVisitor", { "IBAN", "DE12660623660000005703"}) | (*predicates)("EqualVisitor", { "Year", "2023"}) | (*predicates)("EqualVisitor", { "Entry", "501000000891/Grundsteuer"});
+		auto cv = (*visitors)("Accumulation","100");
+		cv = mP.Accept(std::move(cv));
+		std::cout<<"MV"<<mP<<
+        (cv->As<AccumulationVisitor>())()<<std::endl;
+		return Q(5);
+	}
+	virtual std::string_view Name() const { return name; };
+private:
+	std::string name;
+	PredicateFactory predicates;
+	VisitorFactory visitors;
+	template<typename U> using IsT =  Is<U,TypeId>;
+	friend std::ostream& operator<<(std::ostream& s, const MatrixComposition& m) 
+	{ 
+		s<<"Name: "<<m.name<<std::endl;
+		return s;  
+	}
+};
