6db6c052759cc898b8512f1fe0d215856a30f588
markus <markus@gmx.de>
2024-02-04 17:25:12 +0100

    Path

--- Counter/ICounter.hpp
+++ Counter/ICounter.hpp
@@ -23,12 +23,11 @@ class Counter: public ICounter
 	using Type = Counter<Desc>;
 public:
 	Counter(std::string s, std::shared_ptr<Factory<IToken>> tf): Counter{DescriptionType::Make(tf,s),tf} {}
-	Counter(std::unique_ptr<DescriptionType> d, std::shared_ptr<Factory<IToken>> tf): description{std::move(d)}, tokenFactory{tf}, dataModel{description->Read()}
+	Counter(std::unique_ptr<DescriptionType> d, std::shared_ptr<Factory<IToken>> tf): descriptor{std::move(d)}, tokenFactory{tf}, dataModel{descriptor->Read()}
 	{
 		auto elementTokens = (*tokenFactory)({{"SumToken"},{"EntryToken"},{"DateToken"},{"WorkToken"},{"VolumeToken"},{"ValueToken"}, {"EmptyToken"}});
         Matcher matcher(std::move(elementTokens));
-		std::string f = "/home/markus/Downloads/CSV_TestFiles_2/BHot.csv";
-		auto mvr = MatrixReader(f);
+		auto mvr = MatrixReader(descriptor->Path());
         auto mv = mvr.template M<2>();
 		dataModel = std::make_unique<DataModel>(mv.Parse(matcher));
 	}
@@ -36,7 +35,7 @@ public:
 	decltype(auto) To() const { return dataModel->template To<T>(); }
 	decltype(auto) Accept(std::unique_ptr<BaseVisitor> bp) {  return dataModel->Accept(std::move(bp));   }
 private:
-	std::unique_ptr<DescriptionType> description;
+	std::unique_ptr<DescriptionType> descriptor;
 	std::unique_ptr<DataModel> dataModel;
 	std::shared_ptr<Factory<IToken>> tokenFactory;
 	friend std::ostream& operator<<(std::ostream& s, const Counter& c) { return c.display(s);	}
