5b9f4f9abb7c12f4191a57018e13a221bd7719bd
markus <markus@gmx.de>
2024-02-09 18:06:39 +0100

    reader

--- Counter/CounterIO.hpp
+++ Counter/CounterIO.hpp
@@ -22,20 +22,14 @@ public:
 	using Type = T;
 	using DataModel = typename T::DataModel;
 	using Unit = typename T::Unit;
-	CounterIO(std::shared_ptr<Factory<IToken>> f, const std::string& p): tokenFactory{f}, path{p} {}
-//	inline static constexpr uint Number = No;
-//	inline static constexpr const char* Entity = E::Name;
-//	inline static constexpr const char* Stage = S::Value;
-//	inline static std::string Identifier = std::string(Entity) + std::string(D::Value) + "_" + std::string(Stage) + "_" + std::to_string(No);
-//	static std::unique_ptr<CounterIO> Make(std::shared_ptr<Factory<IToken>> f,const std::string& s) { return std::make_unique<CounterIO>(f,s);	}
-//	virtual const std::string& Path() const { return path; };
+	CounterIO(std::shared_ptr<Factory<IToken>> f, const std::string& p): tokenFactory{f}, path{p}, reader{std::make_unique<MatrixReader<std::string>>(path)} {}
 private:	
-	std::string path;
 	std::shared_ptr<Factory<IToken>> tokenFactory;
+	std::string path;
+    std::unique_ptr<MatrixReader<std::string>> reader;   
 	virtual std::unique_ptr<DataModel> read() const 
 	{
-        auto mvr = MatrixReader(path);   
-        auto mv = mvr.template M<2>();
+        auto mv = (*reader).template M<2>();
         auto elementTokens = (*tokenFactory)({{"DateToken"},{ Unit::TokenName }});
         Matcher matcher(std::move(elementTokens));
         return std::make_unique<DataModel>(mv.Parse(matcher));
