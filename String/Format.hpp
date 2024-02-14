763df53e87d6e00976719a98e2008ef2c26aa13e
markus <markus@gmx.de>
2024-02-14 15:02:27 +0100

    SourceLo

--- String/Format.hpp
+++ String/Format.hpp
@@ -2,6 +2,7 @@
 #include<array> 
 #include<iostream> 
 #include<sstream> 
+#include<source_location> 
 #include "../Logger/Logger.hpp"
 #include "../Wrapper/Wrapper.hpp"
 #include "../String/Literal.hpp"
@@ -19,11 +20,12 @@ static decltype(auto) format(std::stringstream& s, auto t, auto... ts)
 class Format
 {	
 public:
-	Format(auto... ts): value{exec(ts...)} {}
+	Format(auto... ts): value{exec(ts...)}, loc {std::source_location::current()} {}
 	operator std::string() const { return value; }
 private:
-	friend  std::ostream& operator<<(std::ostream& out, const Format& f) { return out<<f.value; }
+	friend  std::ostream& operator<<(std::ostream& out, const Format& f) { return out<<f.loc.function_name()<<f.value; }
 	std::string value;
+	std::source_location loc;
 	static auto exec(auto... ts) 
 	{
 		std::stringstream s;
