a98e26e72a5dbf9bb027ef2c0a09033e0dfd1166
markus <markus@gmx.de>
2024-02-15 13:53:31 +0100

    Ref

--- Matrix/MatrixVisitorTest/Readings.hpp
+++ Matrix/MatrixVisitorTest/Readings.hpp
@@ -43,29 +43,9 @@ public:
 	Readings(std::shared_ptr<Factory<IToken>> fT,std::shared_ptr<Factory<IElement>> fE,std::shared_ptr<Factory<BaseVisitor>> fB): XBase{fT,fE,fB} {};
 	decltype(auto) operator()()
 	{
-	    using MDS2 = MatrixDescriptor<2,std::string>;
-	    using MDE1 = MatrixDescriptor<1,std::shared_ptr<IElement>>;
-	    using MS2 = Matrix<2,MDS2>;
-	    using ME1 = Matrix<2,MDE1>;
-	    using TF = TypeFactory<CompositeFactory<IPredicateVisitor, Factory<IElement>>, EqualVisitor, LessVisitor>;
-		using EVF = Factory<BaseVisitor>;
-
-		auto elementTokens = (*tokenFactory)({{"SumToken"},{"EntryToken"},{"DateToken"},{"WorkToken"},{"VolumeToken"},{"ValueToken"}, {"EmptyToken"}});
-        Matcher matcher(std::move(elementTokens));
-
-		auto fmt=std::make_shared<Factory<IElement>>();
-        auto reg = Registration<Factory<IElement>,Quantity<Energy, KiloHour>, Date, Name, Year, Index<int>, Entry, Empty>(&(*fmt));
-        
 		auto fbv = std::make_shared<Factory<BaseVisitor>>();
         auto reg3 = Registration<Factory<BaseVisitor>,DifferenceVisitor<Quantity<Energy, KiloHour>>,DifferenceVisitor<Date>, AccumulationVisitor<Quantity<Volume>>>(&(*fbv));
-        auto cv = (*fbv)("Difference","100");
 
-        auto pfs = std::make_shared<CompositeFactory<IPredicateVisitor, Factory<IElement>>>(fmt);
-        pfs->Register("EQ",[](std::unique_ptr<IElement> e) { return std::make_unique<EqualVisitor>(std::move(e)); });
-        auto regC = Registration<CompositeFactory<IPredicateVisitor, Factory<IElement>>,EqualVisitor>(&(*pfs));
-
-        auto tf = TypeFactory<Factory<IElement>, Quantity<Energy, KiloHour>, IBAN, Date, BIC, ID<std::string>, Name, Index<int>, Empty>();
-        auto tfc = std::make_shared<TF>(fmt);
 		Builder<ICounter,Counter,TopHotDesc, TopColdDesc, MiddleHotDesc, MiddleColdDesc, BottomHotDesc, BottomColdDesc> b;
 		auto cV = b("/home/markus/Downloads/CSV_TestFiles_2", tokenFactory);
 
