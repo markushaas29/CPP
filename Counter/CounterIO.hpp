567eb277678c6bcca2b3bcf29aaa452455f3da0c
markus <markus@gmx.de>
2024-02-19 14:25:57 +0100

    CounterRef

--- Counter/CounterIO.hpp
+++ Counter/CounterIO.hpp
@@ -23,18 +23,9 @@ public:
 	using Type = T;
 	using DataModel = typename T::DataModel;
 	using Unit = typename T::Unit;
-	CounterIO(std::shared_ptr<Factory<IToken>> f, const std::string& p): tokenFactory{f}, path{p}, parser{std::make_unique<CounterParser<Type>>(f,path)}, reader{std::make_unique<MatrixReader<std::string>>(path)} {}
+	CounterIO(std::shared_ptr<Factory<IToken>> f, const std::string& p): parser{std::make_unique<CounterParser<Type>>(f,p)} {}
 private:	
-	std::shared_ptr<Factory<IToken>> tokenFactory;
-	std::string path;
-    std::unique_ptr<MatrixReader<std::string>> reader;   
     std::unique_ptr<IMatrixParser<2>> parser;   
-	virtual std::unique_ptr<DataModel> read() const 
-	{
-        auto mv = (*reader).template M<2>();
-        auto elementTokens = (*tokenFactory)({{"DateToken"},{ Unit::TokenName }});
-        Matcher matcher(std::move(elementTokens));
-        return std::make_unique<DataModel>(mv.Parse(matcher));
-    }
+	virtual std::unique_ptr<DataModel> read() const {  return std::make_unique<DataModel>((*parser)()); }
 	virtual std::ostream& display(std::ostream& out) const { 	return out;	}
 };
