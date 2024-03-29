f531da55c58ce48025988f36c149792f73fb66ba
markus <markus@gmx.de>
2024-03-29 14:22:20 +0100

    Out

--- Matrix/MatrixFormatter.hpp
+++ Matrix/MatrixFormatter.hpp
@@ -8,52 +8,54 @@ template<typename M>
 class MatrixFormatter 
 {
 public:
-	MatrixFormatter() {}
+	MatrixFormatter(const M& m): matrix(m){}
     virtual std::unique_ptr<std::ofstream> operator()(std::unique_ptr<std::ofstream> s) { return s; }
-    virtual std::unique_ptr<std::ofstream> operator()(std::unique_ptr<std::ofstream> s, const M* m) 
-	{ 
-		if constexpr (M::Order==1)
-		{
-			for(auto i=0; i<m->Rows(); ++i)
-				if constexpr (PointerConcept<decltype(*(m->elements->at(i)))>)
-					*s<<*(*(m->elements->at(i)))<<(i+1 == m->Rows()? "" : ", ");
-				else
-					*s<<*(m->elements->at(i))<<(i+1 == m->Rows()? "" : ", ");
-			return s;
-		}
-		else
-		{
-			for(auto i = 0; i != m->Rows(); ++i)
-			{
-				s = (*m)[i].file(std::move(s));
-				*s<<std::endl;
-			}
-		}
-		return s;
-	};
-	virtual std::ostream& operator()(std::ostream& s) {	return s;	}
-    virtual std::ostream& operator()(std::ostream& s, const M* m) 
-	{ 
-		if constexpr (M::Order==1)
-		{
-			s<<"{";
-			for(auto i=0; i<m->Rows(); ++i)
-				if constexpr (PointerConcept<decltype(*(m->elements->at(i)))>)
-					s<<*(*(m->elements->at(i)))<<(i+1 == m->Rows()? "" : ", ");
-				else
-					s<<*(m->elements->at(i))<<(i+1 == m->Rows()? "" : ", ");
-			return s<<" }";
-		}
-		else
-		{
-			s<<"{\n";
-			for(auto i = 0; i != m->Rows(); ++i)
-				in(s)<<(*m)[i]<<std::endl;
-			s<<"}";
-		}
-		return s;
-	};
+//    virtual std::unique_ptr<std::ofstream> operator()(std::unique_ptr<std::ofstream> s, const M* m) 
+//	{ 
+//		if constexpr (M::Order==1)
+//		{
+//			for(auto i=0; i<m->Rows(); ++i)
+//				if constexpr (PointerConcept<decltype(*(m->elements->at(i)))>)
+//					*s<<*(*(m->elements->at(i)))<<(i+1 == m->Rows()? "" : ", ");
+//				else
+//					*s<<*(m->elements->at(i))<<(i+1 == m->Rows()? "" : ", ");
+//			return s;
+//		}
+//		else
+//		{
+//			for(auto i = 0; i != m->Rows(); ++i)
+//			{
+//				s = (*m)[i].file(std::move(s));
+//				*s<<std::endl;
+//			}
+//		}
+//		return s;
+//	};
+//	virtual std::ostream& operator()(std::ostream& s) {	return s;	}
+//    virtual std::ostream& operator()(std::ostream& s, const M* m) 
+//	{ 
+//		if constexpr (M::Order==1)
+//		{
+//			s<<"{";
+//			for(auto i=0; i<m->Rows(); ++i)
+//				if constexpr (PointerConcept<decltype(*(m->elements->at(i)))>)
+//					s<<*(*(m->elements->at(i)))<<(i+1 == m->Rows()? "" : ", ");
+//				else
+//					s<<*(m->elements->at(i))<<(i+1 == m->Rows()? "" : ", ");
+//			return s<<" }";
+//		}
+//		else
+//		{
+//			s<<"{\n";
+//			for(auto i = 0; i != m->Rows(); ++i)
+//				in(s)<<(*m)[i]<<std::endl;
+//			s<<"}";
+//		}
+//		return s;
+//	};
 private:
+	const M matrix;
+	friend std::ostream& operator<<(std::ostream& s, const MatrixFormatter& m) { return s<<m.matrix; }
     std::ostream& in(std::ostream& s)
 	{
 		for(auto i=0; i<M::Order; ++i)
@@ -61,3 +63,6 @@ private:
 		return s;
 	}
 };
+
+template<typename M>
+MatrixFormatter(const M&) -> MatrixFormatter<M>;
