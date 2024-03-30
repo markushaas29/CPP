c1a5bc2143a30ae52db6ae8d1b7caffe1e504e08
markus <markus@gmx.de>
2024-03-30 15:05:36 +0100

    Red

--- Decorator/ElementDecorator.hpp
+++ Decorator/ElementDecorator.hpp
@@ -18,9 +18,15 @@ class Bold: public IElementDecorator
 	inline static constexpr const char TypeIdentifier[] = "Bold";
     inline static constexpr Literal TypeId{TypeIdentifier};
     template<typename U> using IsT =  Is<U,TypeId>;
-public:
-//	Bold(const T& v, const typename Base::DecoratedType& d): Base{std::make_unique<typename Base::DecoratedType>(d)} {}
-//	Bold(const T& v, std::unique_ptr<typename Base::DecoratedType> d): Base{std::move(d)} {}
 private:
 	virtual std::string decorate(const std::string s) const { return "<b>"+s+"<b>"; }
+};
+
+class Red: public IElementDecorator
+{
+	inline static constexpr const char TypeIdentifier[] = "Red";
+    inline static constexpr Literal TypeId{TypeIdentifier};
+    template<typename U> using IsT =  Is<U,TypeId>;
+private:
+	virtual std::string decorate(const std::string s) const { return "<r>"+s+"<r>"; }
 };
--- Decorator/Test.cpp
+++ Decorator/Test.cpp
@@ -33,9 +33,10 @@ class DecoratorTest
 			std::cout<<*fud<<std::endl;
 
 			auto b = Bold();
+			auto r = Red();
 			auto qm = QM{5};
 
-			std::cout<<"B"<<b(qm.Data())<<std::endl;
+			std::cout<<"B"<<r(b(qm.Data()))<<std::endl;
 
 			std::cout<<"End Decorator"<<std::endl;
 
