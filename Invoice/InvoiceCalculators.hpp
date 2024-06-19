f5a61cc0b662be48a28a28c88cc1a82e65b4f78e
markus <markus@gmx.de>
2024-06-19 15:50:28 +0200

    Year

--- Invoice/InvoiceCalculators.hpp
+++ Invoice/InvoiceCalculators.hpp
@@ -93,7 +93,7 @@ private:
 	//    	fv = m[0].Accept(std::move(fv));
 	//    	auto fV = fv->template As<FuncVisitor<QS,Quantity<SumPerArea>, Mul>>();
 			
-			std::cout<<"HALL\n"<<fC.F()<<std::endl;
+			std::cout<<"HALL\n"<<Mul{Constant{12},fC.F()}()<<std::endl;
 		}
 		return (*parser)()[0];	}
 	
