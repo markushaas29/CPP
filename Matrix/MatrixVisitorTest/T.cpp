cfdffaafad66578cd2f0d6dbb2da48999fa66b1f
markus <markus@gmx.de>
2024-09-30 13:21:31 +0200

    Time_9136

--- Matrix/MatrixVisitorTest/T.cpp
+++ Matrix/MatrixVisitorTest/T.cpp
@@ -3,14 +3,18 @@
 #include <vector> 
 #include <tuple> 
 #include <memory> 
+#include <chrono> 
 #include "Test.cpp"
 
 int main()
 {
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 	MatrixVisitorTest analyzer;
 
 	analyzer.Run();
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << "[ms]" << std::endl;
 	return 0;
 }
 
