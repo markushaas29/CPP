7cbccce749d03bbda1d60fba3802064ed2d6cfb5
markus <markus@gmx.de>
2024-11-16 08:36:50 +0100

    Haas

--- Matrix/MatrixVisitorTest/TemplatizedAll_2023.cpp
+++ Matrix/MatrixVisitorTest/TemplatizedAll_2023.cpp
@@ -91,7 +91,7 @@ class TemplatizedAll_Test2023
 			auto hallM3 = (*hall3)(Year(2023),bM).To<Quantity<Sum>>();
 			std::cout<<"Hall"<<hallM3<<std::endl;
 			std::cout<<"H "<<hall2->Value(Year(2023),bM)<<std::endl;
-			assert(hall3->Value(Year(2023),bM).Equals(Quantity<Sum>{2484},0.01));
+			assert(hall3->Value(Year(2023),bM).Equals(Quantity<Sum>{2052},0.01));
 			assert(hallM3[0][0]().Equals(Quantity<Sum>{2052},0.01));
 			
 			auto propB23 = ExtraCostsCalculator<Bottom>(account, tokenFactory,elementFactory,visitorFactory,path);
