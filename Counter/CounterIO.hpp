6a044db70ba199361c3f1421e041f57a279d3dc6
markus <markus@gmx.de>
2024-12-06 08:51:11 +0100

    Vec

--- Counter/CounterIO.hpp
+++ Counter/CounterIO.hpp
@@ -32,6 +32,26 @@ private:
 	virtual std::unique_ptr<DataModel> read() const 
 	{ 
 		auto dm = std::make_unique<DataModel>((*parser)()); 
+		auto elements = dm->Elements();
+		std::vector<std::shared_ptr<IElement>> newElements;
+		
+		size_t rows = elements.size() / 2;
+		for(int i = 0; i <= rows; i = i + 2)
+		{
+			auto nR = elements[i+1];
+			newElements.push_back(elements[i]);
+			newElements.push_back(elements[i+1]);
+			auto oR = elements[i+3];
+			auto diff = std::make_shared<Quantity<Unit>>((nR->template To<Quantity<Unit>>() - oR->template To<Quantity<Unit>>()));
+			newElements.push_back(std::make_shared<Quantity<Unit>>((nR->template To<Quantity<Unit>>() - oR->template To<Quantity<Unit>>())));
+			std::cout<<"DataModel "<<*oR<<"\t"<<*nR<<"\t"<<*diff<<std::endl;
+		}
+		newElements.insert(newElements.end(), elements.end()-2, elements.end());
+		newElements.insert(newElements.end(), elements.end()-1, elements.end());
+		for(int i = 0; i < newElements.size(); ++i)
+		{
+			std::cout<<"D "<<*newElements[i]<<std::endl;
+		}
 		auto mf1 = MatrixFormatter(*dm);
         HtmlBuilder(DescriptorType::Identifier +".html",descriptor.Path())(mf1());
 		return dm;
