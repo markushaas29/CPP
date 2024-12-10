bcf1671d9f86f636b6d9991ffe5b754bf86b44ed
markus <markus@gmx.de>
2024-12-10 06:02:56 +0100

    Empty

--- CSV/Elements.hpp
+++ CSV/Elements.hpp
@@ -235,7 +235,7 @@ class Empty: public Element<Empty>
 	friend class Element<Empty>;
 public:
     inline static constexpr const char* Identifier = "Empty";
-	Empty(const std::string& c = Identifier): Base(c){ };
+	Empty(const std::string& c = ""): Base(c){ };
 private:
 	inline static std::string check(const std::string& s) { return s; }
 };
