56bda89a448490c805c0eb5fe4b10a4dce20c073
markus <markus@gmx.de>
2024-12-27 13:28:26 +0100

    IdPayments

--- Invoice/InvoiceCalculators.hpp
+++ Invoice/InvoiceCalculators.hpp
@@ -253,7 +253,7 @@ private:
 		outs->push_back(std::move(div0));
 		
 		auto div1 = std::make_unique<HtmlElements<DivTag>>("Div1","",std::make_unique<Css<Style<Margin,Px<50>>,Style<BackgroundColor,Hex<"f9f9f9">>>>());
-		div1->Add(mf.Html(std::make_unique<HtmlElement<Caption, Header>>(Header("Payments")),nullptr,"Sums", "Payments"));
+		div1->Add(mf.Html(std::make_unique<HtmlElement<Caption, Header>>(Header("Payments")),nullptr,"Sums", "ExtraCosts"));
 		outs->push_back(std::move(div1));
 
 		auto extraCosts = stageproperties[1].Rows(14,15).template To<Quantity<Sum>>();
@@ -271,7 +271,7 @@ private:
 
 		auto annualAdvancePaymentForm = MatrixFormatter(Init(annualAdvancePayment)());
 		auto div5 = std::make_unique<HtmlElements<DivTag>>("Div5","",std::make_unique<Css<Style<Margin,Px<50>>>>());
-		div5->Add(annualAdvancePaymentForm.Html(std::make_unique<HtmlElement<Caption, Header>>(Header("annualStatement"))));
+		div5->Add(annualAdvancePaymentForm.Html(std::make_unique<HtmlElement<Caption, Header>>(Header("annualStatement")),nullptr,"Sums", "Payments"));
 		outs->push_back(std::move(div5));
 
         auto accountM = Costs(y, html);  
