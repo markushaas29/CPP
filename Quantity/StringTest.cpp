530c2cb6fb2f8b7f2aad57b0a203b972d79e7098
markus <markus@gmx.de>
2024-12-28 09:03:26 +0100

    T

--- Quantity/StringTest.cpp
+++ Quantity/StringTest.cpp
@@ -32,7 +32,7 @@ int Run()
     assert(km1_7.Value()==1.7);
     std::cout<<"km1,7: "<<km1_7<<std::endl;
     std::cout<<"km1,7: "<<km1_7.Data()<<std::endl;
-//    assert(km1_7.Data()=="1.7km");
+    assert(km1_7.Data()=="1.7km");
     std::cout<<"km1000_77: "<<km1000_77.Data()<<std::endl;
     assert(km1000_77.Value()==1000.77);
     assert(km1000_77.Data()=="1000.77km");
