b433610856f5d07187f8627df7513ae04dc4c259
markus <markus@gmx.de>
2025-01-11 06:33:44 +0100

    RentalForm

--- Invoice/InvoiceCalculators.hpp
+++ Invoice/InvoiceCalculators.hpp
@@ -225,6 +225,8 @@ private:
 	    baseVisitor = stageproperties[1].Accept(std::move(baseVisitor));
 		auto addressElements = baseVisitor->template Cast<ElementCollector<Prename, Name, Street, StreetNumber, Postcode, Town>>();
 		auto address = Init(addressElements->Elements())().template Transform<2>(3,2);
+		auto inv = Form<S>(MatrixFormatter(address).Lines(),path, std::to_string(S::Index)+"_Form");
+		inv.exec();
 		
 		std::unique_ptr<BaseVisitor> baseVisitor2 = std::make_unique<ElementCollector<Quantity<Sum>>>();
 	    baseVisitor2 = stageproperties[1].Accept(std::move(baseVisitor2));
