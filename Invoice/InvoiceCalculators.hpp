246a70f8452d445fd0343ecb9b6e28e7af731c21
markus <markus@gmx.de>
2025-01-14 13:45:36 +0100

    Entries

--- Invoice/InvoiceCalculators.hpp
+++ Invoice/InvoiceCalculators.hpp
@@ -97,7 +97,7 @@ private:
 				name.push_back(n->Data());
 		
 		auto q = Quantity<Sum>{0};
-		std::vector<std::vector<std::shared_ptr<IElement>>> elements;
+		std::vector<std::vector<std::shared_ptr<IElement>>> elements = {{std::make_shared<Header>("sumPerArea"),std::make_shared<Header>("Width"),std::make_shared<Header>("Length"),std::make_shared<Header>("Calc"),std::make_shared<Header>("Rent"),std::make_shared<Header>("Calc"),std::make_shared<Header>("Area")}};
 		auto html = HtmlBuilder(std::string(S::Name)+"_Hall.html","/home/markus/Dokumente/cpp/CSV_Files");
 		auto divs = std::make_unique<HtmlElements<DivTag>>("Div1","",std::make_unique<Css<Style<BackgroundColor,Hex<"ffffff">>>>());
 		divs->Add(Html<DivTag>(Header{"Mietvetrag"},std::make_unique<Css<Style<FontWeight,Bold>, Style<MarginTop,Px<100>>, Style<FontSize,Px<25>>>>(),"Header", "Header"));
@@ -142,14 +142,6 @@ private:
 
 			
 				
-				std::vector<std::vector<std::shared_ptr<IElement>>> areas ={
-					{std::make_shared<Entry>("Area"),Date::Today().Clone()},
-					{std::make_shared<Prename>("€/m²"), std::make_shared<Empty>("")},
-					{std::make_shared<Prename>("Rent"), sum.Clone()}
-				};
-				divs->Add(MatrixFormatter(Init(areas)()).Lines(std::make_unique<Css<Style<PaddingTop,Px<180>>,Style<FontSize,Px<25>>>>()));
-				
-
 				auto outs = std::make_unique<std::vector<std::unique_ptr<IHtmlElement>>>();
 				auto classCss = std::make_unique<StyleElement>();
 				classCss->Add(std::make_unique<ClassCss<Style<Padding,Px<14>>>>("border"));
@@ -163,6 +155,14 @@ private:
 				html(grid);
 			}
 		}
+//		std::vector<std::vector<std::shared_ptr<IElement>>> areas ={
+//			{std::make_shared<Entry>("Area"),Date::Today().Clone()},
+//			{std::make_shared<Prename>("€/m²"), std::make_shared<Empty>("")},
+//			{std::make_shared<Prename>("Rent"), sum.Clone()}
+//		};
+		divs->Add(MatrixFormatter(Init(elements)()).Lines(std::make_unique<Css<Style<PaddingTop,Px<180>>,Style<FontSize,Px<25>>>>()));
+				
+
 	
 		auto inv = Form<S>(MatrixFormatter(address).Lines(),std::move(divs),path, std::string(S::Name));
 		inv.exec();
