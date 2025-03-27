5df3461801f23953063182ef8e3b255a7141a366
markus <markus@gmx.de>
2025-03-27 15:07:54 +0100

    get

--- Matrix/MatrixAccess.hpp
+++ Matrix/MatrixAccess.hpp
@@ -279,30 +279,26 @@ private:
 	{
 		using MDT = MatrixDescriptor<Order, typename M::ElementType>;
 		std::array<size_t,Order> e;
-		std::array<size_t,Order> s;
 		std::copy(m->descriptor.Extents().begin(), m->descriptor.Extents().end(), e.begin());
 		std::vector<typename MDT::DataType> result;
 		int i = 0;
+		size_t found = 0;
 		if constexpr (PointerConcept<typename M::ElementType>)
-			std::for_each(m->elements->cbegin(), m->elements->cend(), [&](auto e)
+			std::for_each(m->elements->cbegin(), m->elements->cend(), [&](auto el)
 					{
 						if constexpr (std::is_same_v<typename M::ElementType, std::shared_ptr<IElement>>)
 						{
 								auto v = EqualVisitor(std::make_unique<Date>(29,9,1986));
-								if((*e)->Is(v))
+								if((*el)->Is(v))
 								{
 									auto r = rows<1>({i / m->Cols()},m);
 									auto r2 = row(i / m->Cols(),m);
-									std::cout<<"FOUND!!!!!!!!!!!"<<r<<std::endl;
-
-								//	std::for_each(r2.begin(), r2.end(),[&result](auto re) { result.push_back(re); }); 
+									std::for_each(r2.begin(), r2.end(),[&result](auto re) { result.push_back(re); }); 
+									e[0] = ++found;
 								}
-
-								result.push_back(e); 
-								
 						}
 						else
-    						if (auto ptr = std::dynamic_pointer_cast<T>(*e))
+    						if (auto ptr = std::dynamic_pointer_cast<T>(*el))
 								result.push_back(std::make_shared<T>(*ptr)); 
 							else
 								result.push_back(std::make_shared<T>("")); 
