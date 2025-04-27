db2aaa535b1fa0f4747dadd0eef831e8475f0773
markus <markus@gmx.de>
2025-04-27 14:13:57 +0200

    D

--- Matrix/MatrixParsers.hpp
+++ Matrix/MatrixParsers.hpp
@@ -20,7 +20,7 @@
 
 #pragma once
 
-class Comdirect;
+//class Comdirect;
 
 class IBaseMatrixParser
 {
@@ -194,21 +194,21 @@ private:
 		std::unique_ptr<BaseVisitor> v = std::make_unique<ElementCollector<Date>>();
 		
 		auto m = matrix().Parse(Matcher(std::move(csvIndexTokens)), Matcher(std::move(elementIndexTokens)));
-		m = m.Apply([&](const auto& e1){ 
-			auto ev = IsElementVisitor<Entry>();
-			std::vector<std::shared_ptr<IElement>> v;
-			if((*e1)->Is(ev))
-			{
-				auto entry = (*e1)->template As<Entry>(); 
-				auto entries = entry.template GetElements<Comdirect>();
-				while(entries.size()<=3)
-					entries.push_back(std::make_shared<Text>("-"));
-				v.insert(v.end(),entries.cbegin(), entries.cend());
-			}
-	
-			
-			return v;
-			});
+//		m = m.Apply([&](const auto& e1){ 
+//			auto ev = IsElementVisitor<Entry>();
+//			std::vector<std::shared_ptr<IElement>> v;
+//			if((*e1)->Is(ev))
+//			{
+//				auto entry = (*e1)->template As<Entry>(); 
+//				auto entries = entry.template GetElements<Comdirect>();
+//				while(entries.size()<=3)
+//					entries.push_back(std::make_shared<Text>("-"));
+//				v.insert(v.end(),entries.cbegin(), entries.cend());
+//			}
+//	
+//			
+//			return v;
+//			});
 
 		std::cout<<"Size COllect: \t"<<std::endl;
 	 	v = m.Collect(std::move(v));
