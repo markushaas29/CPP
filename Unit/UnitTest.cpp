0cd73d8ffa17c618bcd8cc5fd14ded222cc56244
markus <markus@gmx.de>
2025-07-07 06:54:54 +0200

    Num

--- Unit/UnitTest.cpp
+++ Unit/UnitTest.cpp
@@ -87,11 +87,15 @@ int Run()
 	assert(decltype(ur12)::URatio()=="min³h^-2");
  	
 	auto urL = TransformRatio<UnitRatio<0,2,0,1>, UnitRatio<0,0,0,0,0,1>, MultiplyPolicy>::Type();
-	std::cout<<"Ratio: "<<TransformRatio<UnitRatio<0,2,0,1>, UnitRatio<0,0,0,0,0,1>, MultiplyPolicy>::R::SiUnit()<<std::endl;
+	assert((PowUnit<std::abs(std::abs(1)-1),decltype(urL)>::Type().URatio())=="h²\"l");
 	assert((TransformRatio<UnitRatio<0,2,0,1>, UnitRatio<0,0,0,0,0,1>, MultiplyPolicy>::R::SiUnit())=="m^-4s^-8");
-	std::cout<<"Ratio: "<<decltype(urL)::Hours::Num<<std::endl;
 	assert(decltype(urL)::Hours::N==2);
 	assert(decltype(urL)::Hours::Num==12960000);
+	assert(decltype(urL)::Hours::Denom==1);
+	assert(decltype(urL)::Liter::Num==1);
+	assert(decltype(urL)::Liter::Denom==1000);
+	assert(decltype(urL)::Inch::Num==254);
+	assert(decltype(urL)::Inch::Denom==10000);
 	
 	auto lpH = TransformRatio<UnitRatio<0,-1>, UnitRatio<0,0,0,0,0,1,0>, MultiplyPolicy>::Type();
 	std::cout<<"Liter / h: "<<(PowUnit<std::abs(std::abs(1)-1),decltype(lpH)>::Type().URatio())<<std::endl;
