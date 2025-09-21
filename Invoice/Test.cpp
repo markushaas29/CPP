945ce5cd87542c5040cfce5aeac43bf450b8cc3e
markus <markus@gmx.de>
2025-09-21 13:36:41 +0200

    Test

---
+++ Invoice/Test.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cassert> 
+#include <vector> 
+#include <tuple> 
+#include <memory> 
+#include "../Matrix/Matrix.hpp"
+#include "../Matrix/MatrixReader.hpp"
+#include "../Matrix/MatrixDescriptor.hpp"
+#include "../Matrix/MatrixProjector.hpp"
+#include "../Matrix/MatrixFilter.hpp"
+#include "../Matrix/MatrixComposition.hpp"
+#include "../Matrix/MatrixElement.hpp"
+#include "../Matrix/M3.hpp"
+#include "../ObjectFactory/Factory.hpp"
+#include "../Common/DateTimes.hpp"
+#include "../CSV/Elements.hpp"
+#include "../CSV/Element.hpp"
+#include "../CSV/Matcher.hpp"
+#include "../Quantity/Quantity.hpp"
+#include "../Functional/Functional.hpp"
+#include "../Common/DateTimes.hpp"
+#include "../Visitor/CollectorVisitor.hpp"
+#include "../Invoice/Invoice.hpp"
+#include "../TypeCounter/TypeCounter.hpp"
+
+class InvoiceTest
+{
+	public:
+		int Run()
+		{
+			std::cout<<"Start Invoice"<<std::endl;
+			auto b1 = std::chrono::steady_clock::now();
+		    using MDS2 = MatrixDescriptor<2,std::string>;
+		    using MS2 = Matrix<2,MDS2>;
+		    using TF = TypeFactory<CompositeFactory<IPredicateVisitor, Factory<IElement>>, EqualVisitor, LessVisitor>;
+			using EVF = Factory<BaseVisitor>;
+
+            auto tokenFactory = TokenFactoryCreator()();
+            auto stageIndexTokens = (*tokenFactory)({{"NameIndexToken"},{"StageIndexToken"},{"WasteIndexToken"},{"HeatingIndexToken"},{"CleaningIndexToken"},{"SewageIndexToken"},{"PropertyTaxIndexToken"},{"InsuranceIndexToken"},{"RentIndexToken"},{"ExtraCostsIndexToken"},{"HeatExtraCostsIndexToken"} });
+            Matcher smatcher(std::move(stageIndexTokens));
+            auto csvIndexTokens = (*tokenFactory)({{"SumIndexToken"},{"IBANIndexToken"},{"DateIndexToken"},{"BICIndexToken"},{"NameIndexToken"}, {"VerwendungszweckIndexToken"}});
+            Matcher imatcher(std::move(csvIndexTokens));
+
+            auto v = (*tokenFactory)({{"SumToken"},{"IBANToken"},{"DateToken"},{"EmptyToken"},{"ValueToken"},{"EntryToken"},{"ScalarToken"}});
+            Matcher matcher(std::move(v));
+
+            auto elementFactory = Build<IElement,Quantity<Sum>, IBAN, Date, BIC, ID<std::string>, Name, Year, Month,Index<int>, Entry,Empty>();
+            auto typeFactory = std::make_shared<TF>(elementFactory);
+
+			auto visitorFactory = std::make_shared<Factory<BaseVisitor>>();
+            auto reg3 = Registration<Factory<BaseVisitor>,AccumulationVisitor<>,AccumulationVisitor<Quantity<Volume>>,ConsumptionVisitor<Quantity<Volume>>>(&(*visitorFactory));
+
+			using AllStages = std::tuple<Bottom, Middle, Top>;
+
+			const std::string path = "/home/markus/Dokumente/cpp/CSV_Files"; 
+			auto account = std::make_shared<AccountCalculator>(tokenFactory,elementFactory,visitorFactory, path);
+			std::chrono::steady_clock::time_point e1 = std::chrono::steady_clock::now();
+
+			std::cout << "Calculation Time Init :"<< std::chrono::duration_cast<std::chrono::milliseconds> (e1 - b1).count() << "[ms]" << std::endl;
+			
+			auto bM = HtmlBuilder<German>("Middle_T_2003.html");
+			
+			auto hall = std::make_unique<Hall<HallOne>>(tokenFactory,elementFactory,visitorFactory,path);
+			auto hallM = (*hall)(Year(2023),bM).To<Quantity<Sum>>();
+			assert(hallM[0][0]().Equals(Quantity<Sum>{680.40},0.01));
+			assert(hallM[0][1]().Equals(Quantity<Sum>{2653.2},0.01));
+			assert(hall->Value(Year(2023),bM).Equals(Quantity<Sum>{3333.6},0.01));
+			
+			auto hall2 = std::make_unique<Hall<HallTwo>>(tokenFactory,elementFactory,visitorFactory,path);
+			auto hallM2 = (*hall2)(Year(2023),bM).To<Quantity<Sum>>();
+			std::cout<<"END 2023"<<std::endl;
+		   
+			return 0;
+		}
+};
