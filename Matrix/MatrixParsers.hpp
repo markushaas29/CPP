b7cbddfaa48ed74c3789cff4bfa2cb681db1e177
markus <markus@gmx.de>
2025-09-24 14:40:32 +0200

    Date

--- Matrix/MatrixParsers.hpp
+++ Matrix/MatrixParsers.hpp
@@ -315,7 +315,7 @@ private:
 	typename Base::MatrixType exec(bool h = false) const
 	{
 		//auto stageIndexTokens = (*tokenFactory)({ {"PrenameIndexToken"},{"NameIndexToken"}, {"StageIndexToken"},{"RentIndexToken"},{"LengthIndexToken"},{"WidthIndexToken"} });
-		auto stageIndexTokens = (*tokenFactory)({{"ProportionIndexToken"},{"SumIndexToken"},{"HeatingIndexToken"},{"CleaningIndexToken"},{"SewageIndexToken"},{"PropertyTaxIndexToken"},{"InsuranceIndexToken"},{"RentIndexToken"},{"ExtraCostsIndexToken"},{"HeatExtraCostsIndexToken"}, {"GarageRentIndexToken"},{"SumPerAreaIndexToken"},{"LengthIndexToken"},{"WidthIndexToken"} });
+		auto stageIndexTokens = (*tokenFactory)({{"ProportionIndexToken"},{"SumIndexToken"},{"NameIndexToken"},{"DateIndexToken"},{"WidthIndexToken"} });
 		return matrix().ParseByMatch(Matcher(std::move(stageIndexTokens)), h);
 	}
 };
