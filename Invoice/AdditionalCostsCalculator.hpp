d3ec7a93e641bcf9fb44735e574cf692a4071d80
markus <markus@gmx.de>
2025-09-25 14:24:59 +0200

    Ca

--- Invoice/AdditionalCostsCalculator.hpp
+++ Invoice/AdditionalCostsCalculator.hpp
@@ -118,6 +118,6 @@ private:
 		auto scalars = m.Cols(0,1,2)[1].To<Quantity<Scalar>>().Elements();
 		std::for_each(scalars.cbegin(),scalars.cend(), [&value](const auto& i) { value = value + i; });
 		std::cout<<m.Cols(0,1,2)[1].To<Quantity<Scalar>>();//.Apply();
-		std::cout<<"\t"<<value;//.Apply();
+		std::cout<<"\t"<<"\t"<<value;//.Apply();
 		return m;	}
 };
