b75a914539940e4938f3a1f5b9ef20bb7e44edad
markus <markus@gmx.de>
2025-09-26 13:44:28 +0200

    AllDesc

--- Invoice/AdditionalCostsCalculator.hpp
+++ Invoice/AdditionalCostsCalculator.hpp
@@ -115,9 +115,9 @@ private:
 //		inv.exec();
 
 		Quantity<Scalar> value{0};
-		auto scalars = m.Cols(0,1,2)[1].To<Quantity<Scalar>>().Elements();
+		auto scalars = m.Cols(0,1,2,3,4,5)[1].To<Quantity<Scalar>>().Elements();
 		std::for_each(scalars.cbegin(),scalars.cend(), [&value](const auto& i) { value = value + i; });
 		std::cout<<m.Cols(0,1,2)[1].To<Quantity<Scalar>>();//.Apply();
-		std::cout<<"\t"<<"\t"<<value;//.Apply();
+		std::cout<<"\n"<<S::Index<<"\n"<<value<<std::endl;//.Apply();
 		return m;	}
 };
