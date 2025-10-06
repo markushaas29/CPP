9323e93c424ddf900d5c2eb51c30b79776c16dcc
markus <markus@gmx.de>
2025-10-06 14:01:51 +0200

    Jansen

--- Invoice/AccountCalculator.hpp
+++ Invoice/AccountCalculator.hpp
@@ -64,6 +64,7 @@ private:
             {"Cleaning",{
                     {"Alles Proper",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE05100110012620778704"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
                     {"Jansen",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE08548500101700257437"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
+                    {"Pfälzer Immobilien Betreuungs GmbH",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE92120300001058229483"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
                     {"Rastaetter",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE79660623660000101303"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
                 }
             },
