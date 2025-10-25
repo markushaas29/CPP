b90f399247942fe02a4ca814aea97ba6b1becaf7
markus <markus@gmx.de>
2025-10-25 12:35:10 +0200

    divs

--- Invoice/HallCalculator.hpp
+++ Invoice/HallCalculator.hpp
@@ -122,8 +122,12 @@ private:
 
 		auto inv = Contract(MatrixFormatter(address).Lines(),std::move(divs),path, std::string(S::Name));
 		inv.exec();
-		
-		auto inv2 = Invoice(MatrixFormatter(address).Lines(),MatrixFormatter(hc).Html(),path, std::string(S::Name)+"_"+std::string(Year{2025}));
+	
+		divs = std::make_unique<HtmlElements<DivTag>>("Div1","",std::make_unique<Css<Style<BackgroundColor,Hex<"ffffff">>>>());
+		divs->Add(Html<DivTag>(Text{"Die Nebenkostenabrechnung Zeitraum: 01.01.2024 bis 31.12.2024 hat abzüglich Ihrer Vorauszahlungen ein Minus von ergeben."},std::make_unique<Css<Style<MarginTop,Px<100>>, Style<FontSize,Px<25>>>>()));
+		divs->Add(MatrixFormatter(hc).Html(std::make_unique<Css<Style<MarginTop,Px<100>>, Style<FontSize,Px<25>>>>()));
+		divs->Add(Html<DivTag>(Text{"Bitte überweisen Sie den Betrag auf das unten genannte Konto."},std::make_unique<Css<Style<MarginTop,Px<100>>, Style<FontSize,Px<25>>>>()));
+		auto inv2 = Invoice(MatrixFormatter(address).Lines(),std::move(divs),path, std::string(S::Name)+"_"+std::string(Year{2025}));
 		inv2.exec();
 
 		std::vector<std::vector<std::shared_ptr<IElement>>> s = {sums, sums};
