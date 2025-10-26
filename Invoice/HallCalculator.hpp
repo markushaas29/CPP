0e767f7cb3ba0ff666c23ff3e727ee21ff864a6d
markus <markus@gmx.de>
2025-10-26 13:19:03 +0100

    Marg

--- Invoice/HallCalculator.hpp
+++ Invoice/HallCalculator.hpp
@@ -124,7 +124,7 @@ private:
 		inv.exec();
 	
 		divs = std::make_unique<HtmlElements<DivTag>>("Div1","",std::make_unique<Css<Style<BackgroundColor,Hex<"ffffff">>>>());
-		divs->Add(Html<DivTag>(Text{"Die Nebenkostenabrechnung Zeitraum: 01.01.2024 bis 31.12.2024 hat abzüglich Ihrer Vorauszahlungen ein Minus von ergeben."},std::make_unique<Css<Style<MarginTop,Px<100>>, Style<FontSize,Px<25>>>>()));
+		divs->Add(Html<DivTag>(Text{"Die Nebenkostenabrechnung Zeitraum: 01.01.2024 bis 31.12.2024 hat abzüglich Ihrer Vorauszahlungen ein Minus von ergeben."},std::make_unique<Css<Style<MarginTop,Px<250>>, Style<FontSize,Px<25>>>>()));
 		divs->Add(MatrixFormatter(hc).Html(std::make_unique<Css<Style<MarginTop,Px<100>>, Style<FontSize,Px<25>>>>()));
 		divs->Add(Html<DivTag>(Text{"Bitte überweisen Sie den Betrag auf das unten genannte Konto."},std::make_unique<Css<Style<MarginTop,Px<100>>, Style<FontSize,Px<25>>>>()));
 		auto inv2 = Invoice(MatrixFormatter(address).Lines(),std::move(divs),path, std::string(S::Name)+"_"+std::string(Year{2025}));
