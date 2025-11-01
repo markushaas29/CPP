0e0c07b0df44080f35dbc576c9b573a17e29bd1c
markus <markus@gmx.de>
2025-11-01 13:53:38 +0100

    Margin

--- Invoice/Form.hpp
+++ Invoice/Form.hpp
@@ -269,8 +269,8 @@ class Invoice: public Form<Raiffeisenbank, NoSignature>
 		divs->Add(Html<Header>(Header{"Invoice"},std::make_unique<Css<Style<MarginTop,Px<250>>, Style<FontWeight,Bold>, Style<FontSize,Px<30>>>>()));
 		divs->Add(Html<DivTag>(Text{"Die Nebenkostenabrechnung Zeitraum: 01.01.2024 bis 31.12.2024 hat abzüglich Ihrer Vorauszahlungen ein Minus von ergeben."},std::make_unique<Css<Style<MarginTop,Px<50>>, Style<FontSize,Px<25>>>>()));
 		//divs->Add((Quantity<Scalar>{1. / 3} * hallCurrent->Value(y)).template HtmlTag<DivTag>(nullptr, std::make_unique<Css<Style<MarginTop,Px<100>>, Style<FontSize,Px<25>>>>()));
-		divs->Add(Html<DivTag>(Text(s.Data()),std::make_unique<Css<Style<MarginTop,Px<100>>, Style<FontSize,Px<25>>>>()));
-		divs->Add(Html<DivTag>(Text{"Bitte überweisen Sie den Betrag auf das unten genannte Konto."},std::make_unique<Css<Style<MarginTop,Px<100>>, Style<FontSize,Px<25>>>>()));
+		divs->Add(Html<DivTag>(Text(s.Data()),std::make_unique<Css<Style<MarginLeft,Px<350>>, Style<MarginTop,Px<150>>, Style<FontSize,Px<25>>>>()));
+		divs->Add(Html<DivTag>(Text{"Bitte überweisen Sie den Betrag auf das unten genannte Konto."},std::make_unique<Css<Style<MarginTop,Px<150>>, Style<FontSize,Px<25>>>>()));
 
 		return divs;
 	}
