d11297c261b655829d21e9348600e0f5c6e5db5a
markus <markus@gmx.de>
2025-11-24 14:27:56 +0100

    Invoice

--- Invoice/Form.hpp
+++ Invoice/Form.hpp
@@ -263,11 +263,11 @@ public:
 
 class Invoice: public Form<Raiffeisenbank, NoSignature> 
 {
-	static auto createContent(const Header& h, const Quantity<Sum>& s)
+	static auto createContent(const Header& h, const Text& t, const Quantity<Sum>& s)
 	{
 		auto divs = std::make_unique<HtmlElements<DivTag>>("Div1","",std::make_unique<Css<Style<BackgroundColor,Hex<"ffffff">>>>());
 		divs->Add(Html<Header>(h,std::make_unique<Css<Style<MarginTop,Px<250>>, Style<FontWeight,Bold>, Style<FontSize,Px<30>>>>()));
-		divs->Add(Html<DivTag>(Text{"Die Nebenkostenabrechnung Zeitraum: 01.01.2024 bis 31.12.2024 hat abzüglich Ihrer Vorauszahlungen ein Minus von ergeben."},std::make_unique<Css<Style<MarginTop,Px<150>>, Style<FontSize,Px<25>>>>()));
+		divs->Add(Html<DivTag>(t,std::make_unique<Css<Style<MarginTop,Px<150>>, Style<FontSize,Px<25>>>>()));
 		//divs->Add((Quantity<Scalar>{1. / 3} * hallCurrent->Value(y)).template HtmlTag<DivTag>(nullptr, std::make_unique<Css<Style<MarginTop,Px<100>>, Style<FontSize,Px<25>>>>()));
 		divs->Add(Html<DivTag>(Text(s.Data()),std::make_unique<Css<Style<MarginLeft,Px<350>>, Style<MarginTop,Px<150>>, Style<FontSize,Px<25>>>>()));
 		divs->Add(Html<DivTag>(Text{"Bitte überweisen Sie den Betrag auf das unten genannte Konto."},std::make_unique<Css<Style<MarginTop,Px<150>>, Style<FontSize,Px<25>>>>()));
@@ -276,5 +276,5 @@ class Invoice: public Form<Raiffeisenbank, NoSignature>
 	}
 public:
 	Invoice(std::unique_ptr<IHtmlElement> a,std::unique_ptr<IHtmlElement> c, const std::string& p, const std::string& n = ""): Form<Raiffeisenbank, NoSignature>(std::move(a), std::move(c), p, n){}
-	Invoice(std::unique_ptr<IHtmlElement> a, const Header& h, const Quantity<Sum>& s,const std::string& p, const std::string& n = ""): Form<Raiffeisenbank, NoSignature>(std::move(a), std::move(createContent(h,s)), p, n){}
+	Invoice(std::unique_ptr<IHtmlElement> a, const Header& h, const Text& t, const Quantity<Sum>& s,const std::string& p, const std::string& n = ""): Form<Raiffeisenbank, NoSignature>(std::move(a), std::move(createContent(h,t,s)), p, n){}
 };
