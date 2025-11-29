0f5326f6ac1ed2c60b64971c5475d7805bcad28a
markus <markus@gmx.de>
2025-11-29 14:32:55 +0100

    20254

--- Matrix/MatrixReaderTest/TemplatizedAll_2023.cpp
+++ Matrix/MatrixReaderTest/TemplatizedAll_2023.cpp
@@ -61,22 +61,22 @@ class MatrixReaderTest
 			auto bM = HtmlBuilder<German>("Middle_T_2003.html");
 			
 			auto hall = std::make_unique<Hall<HallOne>>(tokenFactory,elementFactory,visitorFactory,path);
-			auto hallM = (*hall)(Year(2023),bM).To<Quantity<Sum>>();
+			auto hallM = (*hall)(Year(2025),bM).To<Quantity<Sum>>();
 			assert(hallM[0][0]().Equals(Quantity<Sum>{680.40},0.01));
 			assert(hallM[0][1]().Equals(Quantity<Sum>{2653.2},0.01));
-			assert(hall->Value(Year(2023),bM).Equals(Quantity<Sum>{3333.6},0.01));
+			assert(hall->Value(Year(2025),bM).Equals(Quantity<Sum>{3333.6},0.01));
 			
 			auto hall2 = std::make_unique<Hall<HallTwo>>(tokenFactory,elementFactory,visitorFactory,path);
-			auto hallM2 = (*hall2)(Year(2023),bM).To<Quantity<Sum>>();
+			auto hallM2 = (*hall2)(Year(2025),bM).To<Quantity<Sum>>();
 //			assert(hallM2[0][0]().Equals(Quantity<Sum>{1350},0.01));
 //			assert(hallM2[0][1]().Equals(Quantity<Sum>{990},0.01));
 //			assert(hall2->Value(Year(2023),bM).Equals(Quantity<Sum>{2340},0.01));
 			
 			auto hall3 = std::make_unique<Hall<HallThree>>(tokenFactory,elementFactory,visitorFactory,path);
-			auto hallM3 = (*hall3)(Year(2023),bM).To<Quantity<Sum>>();
+			auto hallM3 = (*hall3)(Year(2025),bM).To<Quantity<Sum>>();
 			std::cout<<"Hall"<<hallM3<<std::endl;
-			std::cout<<"H "<<hall2->Value(Year(2023),bM)<<std::endl;
-			assert(hall3->Value(Year(2023),bM).Equals(Quantity<Sum>{2770.20},0.01));
+			std::cout<<"H "<<hall2->Value(Year(2025),bM)<<std::endl;
+			assert(hall3->Value(Year(2025),bM).Equals(Quantity<Sum>{2770.20},0.01));
 			assert(hallM3[0][0]().Equals(Quantity<Sum>{2770.20},0.01));
 			
 			auto propB23 = ExtraCostsCalculator<Bottom>(account, tokenFactory,elementFactory,visitorFactory,path);
