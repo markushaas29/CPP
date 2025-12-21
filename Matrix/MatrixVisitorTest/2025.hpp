fa22640654d7420c7946775f66ab322586ae3e7b
markus <markus@gmx.de>
2025-12-21 14:21:12 +0100

    Insurance

--- Matrix/MatrixVisitorTest/2025.hpp
+++ Matrix/MatrixVisitorTest/2025.hpp
@@ -93,7 +93,7 @@ class Invoice_2025
 			std::cout<<"mB "<<mB2025[0][4]<<std::endl;
 			assert(mB2025[0][0]().Equals(Quantity<Sum>{-115.45},0.01));
 //			assert(mB2025[0][1]().Equals(Quantity<Sum>{-115.45},0.01));
-//			assert(mB2025[0][2]().Equals(Quantity<Sum>{-115.45},0.01));
+			assert(mB2025[0][2]().Equals(Quantity<Sum>{-539.76},0.01));
 			assert(mB2025[0][3]().Equals(Quantity<Sum>{-629.76},0.01));
 			assert(mB2025[0][4]().Equals(Quantity<Sum>{-252.59},0.01));
 			//assert(mB2025[0][5]().Equals(Quantity<Sum>{-252.59},0.01));
@@ -102,7 +102,7 @@ class Invoice_2025
 			auto mM2025 = propM24(Year{2025}, bM).To<Quantity<Sum>>();
 			assert(mM2025[0][0]().Equals(Quantity<Sum>{-173.18},0.01));
 //			assert(mM2025[0][1]().Equals(Quantity<Sum>{-173.18},0.01));
-//			assert(mM2025[0][2]().Equals(Quantity<Sum>{-173.18},0.01));
+			assert(mM2025[0][2]().Equals(Quantity<Sum>{-539.76},0.01));
 			assert(mM2025[0][3]().Equals(Quantity<Sum>{-629.76},0.01));
 			assert(mM2025[0][4]().Equals(Quantity<Sum>{-252.59},0.01));
 //			assert(mM2025[0][5]().Equals(Quantity<Sum>{-252,59.18},0.01));
@@ -112,7 +112,7 @@ class Invoice_2025
 			std::cout<<"mB "<<mT2025<<std::endl;
 			assert(mT2025[0][0]().Equals(Quantity<Sum>{-57.73},0.01));
 //			assert(mT2025[0][1]().Equals(Quantity<Sum>{-57.73},0.01));
-//			assert(mT2025[0][2]().Equals(Quantity<Sum>{-57.73},0.01));
+			assert(mT2025[0][2]().Equals(Quantity<Sum>{-1079.52},0.01));
 			assert(mT2025[0][3]().Equals(Quantity<Sum>{-1889.28},0.01));
 			assert(mT2025[0][4]().Equals(Quantity<Sum>{-145.05},0.01));
 //			assert(mT2025[0][5]().Equals(Quantity<Sum>{-57.73},0.01));
