1927445f8d11ab7e5290adc75850c48847d7a23f
markus <markus@gmx.de>
2026-01-04 14:26:58 +0100

    House

---
+++ House/House/All.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cassert> 
+#include <vector> 
+#include <tuple> 
+#include <memory> 
+#include "TemplatizedAll_2023.cpp"
+
+int main()
+{
+	TemplatizedAll_Test2023 sut;
+
+	sut.Run();
+
+	return 0;
+}
+
