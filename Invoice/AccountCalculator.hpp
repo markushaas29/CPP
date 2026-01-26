b43980424111e25b7cb8e09e3890959cf45c936f
markus <markus@gmx.de>
2026-01-26 14:39:36 +0100

    AS

--- Invoice/AccountCalculator.hpp
+++ Invoice/AccountCalculator.hpp
@@ -62,7 +62,7 @@ protected:
 
 		auto hb = HtmlBuilder<German>(n + y.ToString() +".html", "/home/markus/Dokumente/cpp/CSV_Files");
 		auto heads = std::make_unique<std::vector<std::unique_ptr<IHtmlElement>>>();
-		heads->push_back(std::make_unique<LinkElement>());
+		heads->push_back(std::make_unique<LinkElement>("AccountStyle"));
 		hb.Add(std::make_unique<HtmlElements<Head>>(std::move(heads)));
         hb.Add(result->Html());
 		hb();
