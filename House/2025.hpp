f8818fb3bf41ab8faa40cf8687d635b11a366285
markus <markus@gmx.de>
2026-01-31 14:55:05 +0100

    Assert

--- House/2025.hpp
+++ House/2025.hpp
@@ -36,14 +36,14 @@ class Invoice_2025
 			auto calculatorT = ExtraCostsCalculator<Top>(account, tokenFactory,elementFactory,visitorFactory,path);
 
 			auto mB2025 = calculatorB(Year{2025}, bM). template To<Quantity<Sum>>();
-			std::cout<<"mB "<<mB2025[0][1]<<std::endl;
+			std::cout<<"mB "<<calculatorB.Value(Year{2025})<<std::endl;
 			assert(mB2025[0][0]().Equals(Quantity<Sum>{-115.45},0.01));
 			assert(mB2025[0][1]().Equals(Quantity<Sum>{-1579.03},0.01));
 			assert(mB2025[0][2]().Equals(Quantity<Sum>{-539.76},0.01));
 			assert(mB2025[0][3]().Equals(Quantity<Sum>{-629.76},0.01));
 			assert(mB2025[0][4]().Equals(Quantity<Sum>{-252.59},0.01));
-//			assert(mB2025[0][5]().Equals(Quantity<Sum>{-252.59},0.01));
-//			assert(calculatorB.Value(Year{2025}).Equals(Quantity<Sum>{-32},0.01));
+			assert(mB2025[0][5]().Equals(Quantity<Sum>{-329.44},0.01));
+			assert(calculatorB.Value(Year{2025}).Equals(Quantity<Sum>{-3446.03},0.01));
 		
 			auto mM2025 = calculatorM(Year{2025}, bM). template To<Quantity<Sum>>();
 			assert(mM2025[0][0]().Equals(Quantity<Sum>{-173.18},0.01));
@@ -51,9 +51,9 @@ class Invoice_2025
 			assert(mM2025[0][2]().Equals(Quantity<Sum>{-539.76},0.01));
 			assert(mM2025[0][3]().Equals(Quantity<Sum>{-629.76},0.01));
 			assert(mM2025[0][4]().Equals(Quantity<Sum>{-252.59},0.01));
-//			assert(mM2025[0][5]().Equals(Quantity<Sum>{-252,59.18},0.01));
-//			assert(calculatorM.Value(Year{2025}).Equals(Quantity<Sum>{-32.42},0.01));
-//
+			assert(mM2025[0][5]().Equals(Quantity<Sum>{-425.57},0.01));
+			assert(calculatorM.Value(Year{2025}).Equals(Quantity<Sum>{-3599.89},0.01));
+
 			auto mT2025 = calculatorT(Year{2025}, bM). template To<Quantity<Sum>>();
 			std::cout<<"mB "<<mT2025<<std::endl;
 			assert(mT2025[0][0]().Equals(Quantity<Sum>{-57.73},0.01));
@@ -61,8 +61,8 @@ class Invoice_2025
 			assert(mT2025[0][2]().Equals(Quantity<Sum>{-1079.52},0.01));
 			assert(mT2025[0][3]().Equals(Quantity<Sum>{-1889.28},0.01));
 			assert(mT2025[0][4]().Equals(Quantity<Sum>{-145.05},0.01));
-//			assert(mT2025[0][5]().Equals(Quantity<Sum>{-57.73},0.01));
-//			assert(calculatorT.Value(Year{2025}).Equals(Quantity<Sum>{-32.42},0.01));
+			assert(mT2025[0][5]().Equals(Quantity<Sum>{-258.15},0.01));
+			assert(calculatorT.Value(Year{2025}).Equals(Quantity<Sum>{-4535.05},0.01));
 			
 			auto hC = std::make_unique<HallCurrentCalculator>(tokenFactory,elementFactory,visitorFactory,path);
 			
