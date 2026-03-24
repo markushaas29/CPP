ecc38ba366d8903e1ad6da9ef931658939c26c2f
markus <markus@gmx.de>
2026-03-24 14:26:33 +0100

    D

--- House/2025.hpp
+++ House/2025.hpp
@@ -64,6 +64,14 @@ class Invoice_2025
 			assert(calculatorB.Value(Year{2025}).Equals(Quantity<Sum>{-3445.06},0.01));
 		
 			auto mM2025 = calculatorM(Year{2025}, bM). template To<Quantity<Sum>>();
+			auto aM26 = calculatorM.AdvancedItems(Year{2025});
+			std::cout<<"mB "<<aM26<<std::endl;
+			assert(aM26[0][0]()->Data()=="Miete");
+			assert(aM26[0][1]()->Data()=="Heiznebenkosten");
+			assert(aM26[0][2]()->Data()=="Nebenkosten");
+			assert(*aM26[1][0]()==Quantity<Sum>{525});
+			assert(*aM26[1][1]()==Quantity<Sum>{0});
+			assert(*aM26[1][2]()==Quantity<Sum>{310});
 			assert(mM2025[0][0]().Equals(Quantity<Sum>{-173.18},0.01));
 			assert(mM2025[0][1]().Equals(Quantity<Sum>{-1579.03},0.01));
 			assert(mM2025[0][2]().Equals(Quantity<Sum>{-539.76},0.01));
