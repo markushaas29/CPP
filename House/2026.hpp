6debf6219ea6ac52fb9f9a4bd0766d7d7c323b5f
markus <markus@gmx.de>
2026-03-26 14:24:22 +0100

    D

--- House/2026.hpp
+++ House/2026.hpp
@@ -11,18 +11,18 @@ class Invoice_2026
 			auto account = std::make_shared<AccountCalculator>(tokenFactory,elementFactory,visitorFactory, path);
 			auto bM = HtmlBuilder<German>("Middle_T_2003.html");
 			
-//			auto hall = std::make_unique<Hall<HallOne>>(tokenFactory,elementFactory,visitorFactory,path);
-//			auto hallM = (*hall)(Year(2026),bM). template To<Quantity<Sum>>();
-//			assert(hallM[0][0]().Equals(Quantity<Sum>{680.40},0.01));
-//			assert(hallM[0][1]().Equals(Quantity<Sum>{2653.2},0.01));
-//			assert(hall->Value(Year(2026),bM).Equals(Quantity<Sum>{3333.6},0.01));
-//			
-//			auto hall2 = std::make_unique<Hall<HallTwo>>(tokenFactory,elementFactory,visitorFactory,path);
-//			(*hall2)(Year(2024),bM). template To<Quantity<Sum>>();
-//			auto hallM2 = (*hall2)(Year(2026),bM). template To<Quantity<Sum>>();
-////			assert(hallM2[0][0]().Equals(Quantity<Sum>{1350},0.01));
-////			assert(hallM2[0][1]().Equals(Quantity<Sum>{990},0.01));
-////			assert(hall2->Value(Year(2023),bM).Equals(Quantity<Sum>{2340},0.01));
+			auto hall = std::make_unique<Hall<HallOne>>(tokenFactory,elementFactory,visitorFactory,path);
+			auto hallM = (*hall)(Year(2026),bM). template To<Quantity<Sum>>();
+			assert(hallM[0][0]().Equals(Quantity<Sum>{680.40},0.01));
+			assert(hallM[0][1]().Equals(Quantity<Sum>{2653.2},0.01));
+			assert(hall->Value(Year(2026),bM).Equals(Quantity<Sum>{3333.6},0.01));
+			
+			auto hall2 = std::make_unique<Hall<HallTwo>>(tokenFactory,elementFactory,visitorFactory,path);
+			(*hall2)(Year(2024),bM). template To<Quantity<Sum>>();
+			auto hallM2 = (*hall2)(Year(2026),bM). template To<Quantity<Sum>>();
+//			assert(hallM2[0][0]().Equals(Quantity<Sum>{1350},0.01));
+//			assert(hallM2[0][1]().Equals(Quantity<Sum>{990},0.01));
+//			assert(hall2->Value(Year(2023),bM).Equals(Quantity<Sum>{2340},0.01));
 			
 //			auto hall3 = std::make_unique<Hall<HallThree>>(tokenFactory,elementFactory,visitorFactory,path);
 //			auto hallM3 = (*hall3)(Year(2026),bM). template To<Quantity<Sum>>();
@@ -31,18 +31,18 @@ class Invoice_2026
 //			assert(hall3->Value(Year(2026),bM).Equals(Quantity<Sum>{2770.20},0.01));
 //			assert(hallM3[0][0]().Equals(Quantity<Sum>{2770.20},0.01));
 
-			auto propB24 = ExtraCostsCalculator<Bottom>(account, tokenFactory,elementFactory,visitorFactory,path);
-			auto propM24 = ExtraCostsCalculator<Middle>(account, tokenFactory,elementFactory,visitorFactory,path);
-			auto propT24 = ExtraCostsCalculator<Top>(account, tokenFactory,elementFactory,visitorFactory,path);
+			auto calculatorB = ExtraCostsCalculator<Bottom>(account, tokenFactory,elementFactory,visitorFactory,path);
+			auto calculatorM = ExtraCostsCalculator<Middle>(account, tokenFactory,elementFactory,visitorFactory,path);
+			auto calculatorT = ExtraCostsCalculator<Top>(account, tokenFactory,elementFactory,visitorFactory,path);
 
-			auto aB26 = propB24.AdvancedItems(Year{2026});
+			auto aB26 = calculatorB.AdvancedItems(Year{2026});
 			assert(aB26[0][0]()->Data()=="Miete");
 			assert(aB26[0][1]()->Data()=="Heiznebenkosten");
 			assert(aB26[0][2]()->Data()=="Nebenkosten");
 			assert(*aB26[1][0]()==Quantity<Sum>{500});
 			assert(*aB26[1][1]()==Quantity<Sum>{135});
 			assert(*aB26[1][2]()==Quantity<Sum>{67});
-			auto mB2026 = propB24(Year{2026}, bM). template To<Quantity<Sum>>();
+			auto mB2026 = calculatorB(Year{2026}, bM). template To<Quantity<Sum>>();
 //			assert(mB2026[0][0]().Equals(Quantity<Sum>{-115.45},0.01));
 //			assert(mB2026[0][1]().Equals(Quantity<Sum>{-115.45},0.01));
 //	  		assert(mB2026[0][2]().Equals(Quantity<Sum>{-539.76},0.01));
@@ -50,15 +50,15 @@ class Invoice_2026
 //	  		assert(mB2026[0][4]().Equals(Quantity<Sum>{-252.59},0.01));
 //	  		//assert(mB2026[0][5]().Equals(Quantity<Sum>{-252.59},0.01));
 	
-			auto aM26 = propM24.AdvancedItems(Year{2026});
+			auto aM26 = calculatorM.AdvancedItems(Year{2026});
 			assert(aM26[0][0]()->Data()=="Miete");
 			assert(aM26[0][1]()->Data()=="Heiznebenkosten");
 			assert(aM26[0][2]()->Data()=="Nebenkosten");
 			assert(*aM26[1][0]()==Quantity<Sum>{600});
 			assert(*aM26[1][1]()==Quantity<Sum>{0});
 			assert(*aM26[1][2]()==Quantity<Sum>{310});
-	  		auto mM2026 = propM24(Year{2026}, bM). template To<Quantity<Sum>>();
-			auto costs = propM24.AdvancedItems(Year{2026})[1];
+	  		auto mM2026 = calculatorM(Year{2026}, bM). template To<Quantity<Sum>>();
+			auto costs = calculatorM.AdvancedItems(Year{2026})[1];
 //	  		assert(mM2026[0][0]().Equals(Quantity<Sum>{-173.18},0.01));
 //				assert(mM2026[0][1]().Equals(Quantity<Sum>{-173.18},0.01));
 //	  		assert(mM2026[0][2]().Equals(Quantity<Sum>{-539.76},0.01));
@@ -66,7 +66,7 @@ class Invoice_2026
 //	  		assert(mM2026[0][4]().Equals(Quantity<Sum>{-252.59},0.01));
 //				assert(mM2026[0][5]().Equals(Quantity<Sum>{-252,59.18},0.01));
 	
-	  		auto mT2026 = propT24(Year{2026}, bM). template To<Quantity<Sum>>();
+	  		auto mT2026 = calculatorT(Year{2026}, bM). template To<Quantity<Sum>>();
 	  		std::cout<<"mB "<<mT2026<<std::endl;
 //	  		assert(mT2026[0][0]().Equals(Quantity<Sum>{-57.73},0.01));
 //				assert(mT2026[0][1]().Equals(Quantity<Sum>{-57.73},0.01));
