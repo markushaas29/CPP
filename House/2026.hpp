f8b4633d45877040e8a07238f10bb4df75306ebd
markus <markus@gmx.de>
2026-04-21 14:45:43 +0200

    Result

--- House/2026.hpp
+++ House/2026.hpp
@@ -11,8 +11,8 @@ class Invoice_2026
 
 			auto account = std::make_shared<AccountCalculator>(tokenFactory,elementFactory,visitorFactory, path);
 			auto bM = HtmlBuilder<German>("Middle_T_2003.html");
-			auto aq = account->GetYear(y,bM);
-			std::cout<<"\nAQ 2026\n"<<aq<<std::endl;
+			auto aq = account->Result(y,bM);
+			std::cout<<"\nAQ 2026\n"<<*aq<<std::endl;
 			
 			auto hall = std::make_unique<Hall<HallOne>>(tokenFactory,elementFactory,visitorFactory,path);
 			auto hallM = (*hall)(Year(2026),bM). template To<Quantity<Sum>>();
--- Invoice/AccountCalculator.hpp
+++ Invoice/AccountCalculator.hpp
@@ -39,6 +39,29 @@ public:
 		return exec(y,f,allFactoryUnits,n);
 	}
 protected:
+	std::unique_ptr<IResult<Quantity<Unit<1>>, Matrix<2, MatrixDescriptor<2,std::shared_ptr<IElement>>>>, std::default_delete<IResult<Quantity<Unit<1>>, Matrix<2, MatrixDescriptor<2, std::shared_ptr<IElement>>>>>> getResult(const Year& y, const HtmlBuilder<German>& f, const std::vector<FactoryUnitContainer<FactoryUnitContainer<FactoryUnit<std::string,FactoryUnit<std::string, std::string>>>>>& allFactoryUnits, const std::string& n) const
+	{
+		using MDS2 = MatrixDescriptor<2,std::string>;
+        using MS2 = Matrix<2,MDS2>;
+		
+		using TF = TypeFactory<CompositeFactory<IPredicateVisitor, Factory<IElement>>, EqualVisitor, LessVisitor>;
+		auto typeFactory = std::make_shared<TF>(elementFactory);
+		
+		auto parsedAccountMatrix = AccountParser::Instance(tokens,path)();
+
+		auto fu = std::make_shared<std::ofstream>("/home/markus/Downloads/CSV_TestFiles_2/FactoryUnits.txt");
+		for(auto u : allFactoryUnits)
+			(*fu)<<u;
+		fu->close();
+
+		auto all = std::make_unique<MatrixComposite<decltype(parsedAccountMatrix)>>("All");
+
+        std::vector<FactoryUnit<std::string, std::string>> fv{{"AccumulationSum"}};
+        for(uint i = 0; i < allFactoryUnits.size(); ++i)
+                 all->Add(MatrixComposite<decltype(parsedAccountMatrix)>::Create(typeFactory,visitorFactory,std::move(allFactoryUnits[i].Name()), allFactoryUnits[i].Units(),fv));
+        
+		return (*all)(parsedAccountMatrix);
+	}
 	typename Base::MatrixType exec(const Year& y, const HtmlBuilder<German>& f, const std::vector<FactoryUnitContainer<FactoryUnitContainer<FactoryUnit<std::string,FactoryUnit<std::string, std::string>>>>>& allFactoryUnits, const std::string& n) const
 	{
 		using MDS2 = MatrixDescriptor<2,std::string>;
@@ -98,6 +121,7 @@ class AccountCalculator: public AccountCalculatorBase, public IAccount
 public:
 	AccountCalculator(std::shared_ptr<Factory<IToken>> fT,std::shared_ptr<Factory<IElement>> fE,std::shared_ptr<Factory<BaseVisitor>> fB, const std::string& p): Base{fT,fE,fB,p} {};
 	auto GetYear(const Year& y, const HtmlBuilder<German>& f) const { return getTokens(y,f); }
+	auto Result(const Year& y, const HtmlBuilder<German>& f) const { return getTokens2(y,f); }
 private:
 	virtual Matrix<2,MatrixDescriptor<2,std::shared_ptr<IElement>>> get(std::unique_ptr<IAccountQuery> q, const HtmlBuilder<German>& f) const { return getTokens(Year{2025},f,std::move(q)); };
 	virtual Matrix<2,MatrixDescriptor<2,std::shared_ptr<IElement>>> getTokens(const Year& y, const HtmlBuilder<German>& f, std::unique_ptr<IAccountQuery> q = std::make_unique<ExtraCostQuery>()) const
@@ -150,6 +174,57 @@ private:
         };
 		
 		return Base::exec(y,f, (*q)(y),q->Name(y));
+	}
+	std::unique_ptr<IResult<Quantity<Unit<1>>, Matrix<2, MatrixDescriptor<2,std::shared_ptr<IElement>>>>, std::default_delete<IResult<Quantity<Unit<1>>, Matrix<2, MatrixDescriptor<2, std::shared_ptr<IElement>>>>>> getTokens2(const Year& y, const HtmlBuilder<German>& f, std::unique_ptr<IAccountQuery> q = std::make_unique<ExtraCostQuery>()) const
+	{
+		std::vector<FactoryUnitContainer<FactoryUnitContainer<FactoryUnit<std::string,FactoryUnit<std::string, std::string>>>>> allFactoryUnits = 
+        {
+            {"Waste",
+                {
+                
+                    {"Deduction",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE44600501010008017284"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}} // Waste
+                }
+            }, 
+            {"Heating",
+                {
+                    {"Deduction",{{EqualVisitor::Identifier, { Entry::Identifier, "Abschlagsforderung"}}, {EqualVisitor::Identifier, { Entry::Identifier, "701006843905"}}, {EqualVisitor::Identifier, { IBAN::Identifier, "DE56600501017402051588"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
+                    {"Deduction",{{EqualVisitor::Identifier, { Entry::Identifier, "Abschlagsforderung"}}, {EqualVisitor::Identifier, { IBAN::Identifier, "DE68600501010002057075"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
+                    //{"Deduction",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE92600501010004021281"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
+                    {"Deduction",{{EqualVisitor::Identifier, { Entry::Identifier, "Dettenheim/AB/GAS/"}}, {EqualVisitor::Identifier, { IBAN::Identifier, "DE92600501010004021281"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
+                    {"Invoice",{{EqualVisitor::Identifier, { Entry::Identifier, "Rechnung"}}, {EqualVisitor::Identifier, { IBAN::Identifier, "DE56600501017402051588"}}, {EqualVisitor::Identifier, { Month::Identifier, Jan.ToString()}}, {EqualVisitor::Identifier, { Year::Identifier, y.Next().ToString()}}}},
+                    {"Rechnung",{{EqualVisitor::Identifier, { Entry::Identifier, "Rechnung"}}, {EqualVisitor::Identifier, { IBAN::Identifier, "DE68600501010002057075"}}, {EqualVisitor::Identifier, { Year::Identifier, y.Next().ToString()}}}},
+                    {"Rechnung",{{EqualVisitor::Identifier, { Entry::Identifier, "Dettenheim/RE/GAS/"}}, {EqualVisitor::Identifier, { IBAN::Identifier, "DE92600501010004021281"}}, {EqualVisitor::Identifier, { Year::Identifier, y.Next().ToString()}}}},
+                    {"Maintenance",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE73660623660009271112"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
+                    {"Maintenance",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE82660501011021592702"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
+                }
+            },
+            {"BuildingInsurance",{
+                    {"Invoice",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE97500500000003200029"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}} // Insurance
+                }
+            },
+            {"Cleaning",{
+                    {"Alles Proper",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE05100110012620778704"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
+                    {"Jansen",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE08548500101700257437"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
+                    {"Pfälzer Immobilien Betreuungs GmbH",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE92120300001058229483"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
+                    {"Rastaetter",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE79660623660000101303"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}}},
+                }
+            },
+            {"PropertyTax",{
+                    {"Deduction",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE12660623660000005703"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}, {EqualVisitor::Identifier, { Entry::Identifier, "501000000891/Grundsteuer"}}}} //Grundsteuer
+                }
+            },
+            {"Sewage",{
+                    {"Deduction",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE12660623660000005703"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}, {EqualVisitor::Identifier, { Entry::Identifier, "588880002829/Schmutzwasser"}}}}, //Abwasser
+                    {"Deduction",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE12660623660000005703"}}, {EqualVisitor::Identifier, { Year::Identifier, y.ToString()}}, {EqualVisitor::Identifier, { Entry::Identifier, "Abschlag/Abwasser"}}}}, //Abwasser
+                    {"Invoice",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE12660623660000005703"}}, {EqualVisitor::Identifier, { Year::Identifier, y.Next().ToString()}}, {EqualVisitor::Identifier, { Entry::Identifier, "Rechnung/Abwasser"}}}}, //Abwasser
+                    {"Invoice",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE12660623660000005703"}}, {EqualVisitor::Identifier, { Year::Identifier, y.Next().ToString()}}, {EqualVisitor::Identifier, { Entry::Identifier, "588880002829/Schmutzwasse Rechnung 2025"}}}}, //Abwasser
+                    {"Invoice",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE34660501010100000058"}}, {EqualVisitor::Identifier, { Year::Identifier, y.Next().ToString()}}, {EqualVisitor::Identifier, { Entry::Identifier, "/Schmutzwasser 2025 RUCHENSTR. 14/Wasser 2025 RUCHENSTR. 14 EREF: 110900003850"}}}}, //Abwasser
+                    //{"Invoice",{{EqualVisitor::Identifier, { IBAN::Identifier, "DE12660623660000005703"}}, {EqualVisitor::Identifier, { Year::Identifier, y.Next().ToString()}}, {EqualVisitor::Identifier, { Entry::Identifier, "/Schmutzwasser 2025 RUCHENSTR. 14/Wasser 2025 RUCHENSTR. 14 EREF: 110900003850"}}}} //Abwasser
+                }
+            }
+        };
+		
+		return Base::getResult(y,f, (*q)(y),q->Name(y));
 	}
 };
 
